7b17bafe4440524aadddd1c18450e4123330353a
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-04 12:28:24 +0530

    add pages

---
+++ src/Components/movies/Collection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Collection = () => {
+    return (
+        <React.Fragment>
+            <div id="loading"></div>
+            <section class="content">
+                <div class="row">
+                    <div class="col-md-12 movie-wrapper">
+                        <div id="results">Hi</div>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default Collection
