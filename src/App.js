5fa65bb92ebce72f4e355658d7e00a02392b3195
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-05 22:39:22 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react'
-import Header from './Components/Main/Header'
-import Collection from './Components/Movies/Collection'
+import MainComponent from './components/main/MainComponent'
+
+
 
 const App = () => {
   const [theme, setTheme] = useState("lack")
   if (theme === "black") {
-    import('./Components/Styles/react4moviesDark.css')
+    import('./components/styles/react4moviesDark.css')
   }
   else {
-    import('./Components/Styles/react4movies.css')
+    import('./components/styles/react4movies.css')
   }
 
   return (
     <>
-      <div >
-        <Header />
-        <Collection />
-      </div>
-
-
+      <MainComponent />
     </>
   )
 }
