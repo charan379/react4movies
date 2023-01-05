4f4051f572222ce1d61781a515c82b98a36f483c
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-05 16:40:04 +0530

    header updated

--- src/Components/Main/Header.js
+++ src/Components/Main/Header.js
@@ -1,76 +1,26 @@
 import React from 'react'
-
-const Header = ({children}) => {
+import user from '../Static/Icons/user.svg'
+const Header = () => {
     return (
         <>
-            {/* <!-- Image and text --> */}
-            {/* <!-- Navbar --> */}
-            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                {/* <!-- Container wrapper --> */}
-                <div className="container-fluid">
-                    <div>
-                        {/* <!-- Navbar brand --> */}
-                        <a className="navbar-brand" href="#" style={{ float: "left" }}>
-                            <img
-                                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                                className="me-2"
-                                height="20"
-                                alt="MDB Logo"
-                                loading="lazy"
-                            />
-                            <small>MDBootstrap</small>
-                        </a>
-                    </div>
-
-                    {/* <!-- Right elements --> */}
-                    <div className="d-flex align-items-center">
-                        {/* <!-- Icon --> */}
-                        <a className="text-reset me-3" href="#">
-                            <i className="fa fa-shopping-cart"></i>
-                        </a>
-
-
-                        {/* <!-- Avatar --> */}
-                        <div className="dropdown">
-                            <a
-                                className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                                href="#"
-                                id="navbarDropdownMenuAvatar"
-                                role="button"
-                                data-mdb-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <img
-                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                                    className="rounded-circle"
-                                    height="25"
-                                    alt="Black and White Portrait of a Man"
-                                    loading="lazy"
-                                />
-                            </a>
-                            <ul
-                                className="dropdown-menu dropdown-menu-end"
-                                aria-labelledby="navbarDropdownMenuAvatar"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">My profile</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Settings</a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">Logout</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    {/* <!-- Right elements --> */}
+            <nav className='navbar'>
+                <div className='nav-title'>
+                    react4movies
                 </div>
+                <ul className='nav-items'>
+                    {/* <li className='nav-item' >
+                        <i className="fa fa-user-o " aria-hidden="true"></i>
+                    </li>
+                    <li className='nav-item' >
+                        <i className="fa fa-user-o " aria-hidden="true"></i>
+                    </li> */}
+                    <li className='nav-item' >
+                        <img className='nav-img' src={user} ></img>
+                    </li>
+                   
+                    
+                </ul>
             </nav>
-            {/* <!-- Navbar --> */}
-            <br></br>
-            <br></br>
-        {children}
         </>
     )
 }
