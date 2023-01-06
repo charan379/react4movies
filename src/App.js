9a3cc67f2954e11788d8b6415fc18903bc29320c
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-07 00:20:27 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import MainComponent from './components/main/MainComponent'
+import ThemeContextProvider from './components/store/contextAPI/themeToggler/ThemeContext'
+import ToogleTheme from './components/store/contextAPI/themeToggler/ToogleTheme'
 
 
 
 const App = () => {
-  const [theme, setTheme] = useState("lack")
-  if (theme === "black") {
-    import('./components/styles/react4moviesDark.css')
-  }
-  else {
-    import('./components/styles/react4movies.css')
-  }
+  const [theme, setTheme] = useState("light")
+
+  useLayoutEffect(() => {
+    if (theme === "black") {
+      import('./components/styles/react4moviesDark.css')
+    }
+    else {
+      import('./components/styles/react4movies.css')
+    }
+    console.log(theme)
+    return () => {
+
+    };
+
+  }, [theme])
 
   return (
     <>
-      <MainComponent />
+      <ThemeContextProvider>
+        <MainComponent />
+      </ThemeContextProvider>
+
+
     </>
   )
 }
