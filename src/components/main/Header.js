7cc29e4463f6ab3837ed9778b5a1fa9abd5c89d6
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-07 00:20:59 +0530

    header updated

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,14 +1,19 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useContext, useRef, useState } from 'react'
 import useOnOutSideClick from '../hooks/useOnOutSideClick';
 import user from '../static/icons/user.svg'
+import day from '../static/icons/day.svg'
+import night from '../static/icons/night.svg'
+import { ThemeContext } from '../store/contextAPI/themeToggler/ThemeContext';
+import ToogleTheme from '../store/contextAPI/themeToggler/ToogleTheme';
 
 
 const Header = () => {
-    
+    // const {theme} = useContext(ThemeContext);
     //  is dropdown open State
     const [isDropdwonOpen,setDropdownOpen] = useState(false);
     const dropdownRef = useRef();
-    useOnOutSideClick(dropdownRef, useCallback(()=>{setDropdownOpen(false)}))
+    useOnOutSideClick(dropdownRef, useCallback(()=>{setDropdownOpen(false)},[]))
+    const {theme} = useContext(ThemeContext);
     return (
         <>
             <nav className='navbar'>
@@ -25,6 +30,13 @@ const Header = () => {
                     {/* <li className='nav-item' >
                         <img className='nav-img' src={user} ></img>
                     </li> */}
+                    <ToogleTheme className="nav-item">
+                        <li className='nav-item' >
+                        <img className='nav-img' src={theme === "light" ? day : night} ></img>
+                    </li>
+                    </ToogleTheme>
+
+
                     <div ref={dropdownRef} className="navbar-dropdown" onClick={()=>setDropdownOpen(true)}>
                     <li className='nav-item' >
                         <img className='nav-img' src={user} ></img>
