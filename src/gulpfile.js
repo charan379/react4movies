0adc48d31588aab4702159622b5b1bd35cef58ae
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-07 18:03:48 +0530

    automte css minification

---
+++ src/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+function minifyConbineCss(){
+    return gulp.src('./components/styles/react4movies/*.css')
+                .pipe(sourcemaps.init())
+                .pipe(concat('react4movies.min.css'))
+                .pipe(cleanCSS())
+                .pipe(sourcemaps.write('.'))
+                .pipe(gulp.dest('../public'))
+}
+
+exports.css = minifyConbineCss
+exports.default = minifyConbineCss
