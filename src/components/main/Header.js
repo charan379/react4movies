84992e196d46bf33405e543994c5e575c8944ee8
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-07 18:04:21 +0530

    header updated

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,57 +1,72 @@
-import React, { useCallback, useContext, useRef, useState } from 'react'
-import useOnOutSideClick from '../hooks/useOnOutSideClick';
-import user from '../static/icons/user.svg'
-import day from '../static/icons/day.svg'
-import night from '../static/icons/night.svg'
-import { ThemeContext } from '../store/contextAPI/themeToggler/ThemeContext';
-import ToogleTheme from '../store/contextAPI/themeToggler/ToogleTheme';
+import React, { useCallback, useContext, useRef, useState } from "react";
+import useOnOutSideClick from "../hooks/useOnOutSideClick";
+import user from "../static/icons/user.svg";
+import day from "../static/icons/day.svg";
+import night from "../static/icons/night.svg";
+import { ThemeContext } from "../store/contextAPI/themeToggler/ThemeContext";
+import ToogleTheme from "../store/contextAPI/themeToggler/ToogleTheme";
 
-
+// Main Header
 const Header = () => {
-    // const {theme} = useContext(ThemeContext);
-    //  is dropdown open State
-    const [isDropdwonOpen,setDropdownOpen] = useState(false);
-    const dropdownRef = useRef();
-    useOnOutSideClick(dropdownRef, useCallback(()=>{setDropdownOpen(false)},[]))
-    const {theme} = useContext(ThemeContext);
-    return (
-        <>
-            <nav className='navbar'>
-                <div className='nav-title'>
-                    react4movies
-                </div>
-                <ul className='nav-items'>
-                    {/* <li className='nav-item' >
+  // current theme
+  const { theme } = useContext(ThemeContext);
+
+  //  is dropdown open State
+  const [isDropdwonOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef();
+  useOnOutSideClick(
+    dropdownRef,
+    useCallback(() => {
+      setDropdownOpen(false);
+    }, [])
+  );
+  return (
+    <>
+      <nav className={`navbar ${theme}`}>
+        <div className="nav-title">react4movies</div>
+        <ul className="nav-items">
+          {/* <li className='nav-item' >
                         <i className="fa fa-user-o " aria-hidden="true"></i>
                     </li>
                     <li className='nav-item' >
                         <i className="fa fa-user-o " aria-hidden="true"></i>
                     </li> */}
-                    {/* <li className='nav-item' >
+          {/* <li className='nav-item' >
                         <img className='nav-img' src={user} ></img>
                     </li> */}
-                    <ToogleTheme className="nav-item">
-                        <li className='nav-item' >
-                        <img className='nav-img' src={theme === "light" ? day : night} ></img>
-                    </li>
-                    </ToogleTheme>
+          <ToogleTheme className="nav-item">
+            <li className="nav-item">
+              <img
+                className="nav-img"
+                src={theme === "light" ? day : night}
+              ></img>
+            </li>
+          </ToogleTheme>
 
+          <div
+            ref={dropdownRef}
+            className="navbar-dropdown"
+            onClick={() => setDropdownOpen(true)}
+          >
+            <li className="nav-item">
+              <img className="nav-img" src={user}></img>
+            </li>
+            <div
+              className={
+                isDropdwonOpen
+                  ? "navbar-dropdown-content show"
+                  : "navbar-dropdown-content"
+              }
+            >
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-                    <div ref={dropdownRef} className="navbar-dropdown" onClick={()=>setDropdownOpen(true)}>
-                    <li className='nav-item' >
-                        <img className='nav-img' src={user} ></img>
-                    </li>
-                        <div className={isDropdwonOpen ? "navbar-dropdown-content show" : "navbar-dropdown-content"}>
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
-
-                </ul>
-            </nav>
-        </>
-    )
-}
-
-export default Header
+export default Header;
