607894e94df51c03feb8d7815529b414e29b0aac
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-16 21:23:41 +0530

    ROUTING ADDED

--- web_client/src/App.js
+++ web_client/src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainComponent from './components/main/MainComponent'
-import ThemeContextProvider from './components/store/contextAPI/themeToggler/ThemeContext'
+import Collection from './components/movies/Collection'
+import FetchTitle from './components/movies/FetchTitle'
+import Home from './components/movies/Home'
+import AddTitle from './components/movies/AddTitle'
+import ThemeContextProvider from './utils/store/contextAPI/themeToggler/ThemeContext'
 
 
 
@@ -14,7 +19,16 @@ const App = () => {
   return (
     <>
       <ThemeContextProvider>
-        <MainComponent />
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<MainComponent/>}>
+            <Route index element = {<Home/>} />
+            <Route path='/collection' element={<Collection/>} />
+            <Route path='/fetch-title' element={<FetchTitle/>} />
+            <Route path='/add-title' element={<AddTitle />} />
+          </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeContextProvider>
     </>
   )
