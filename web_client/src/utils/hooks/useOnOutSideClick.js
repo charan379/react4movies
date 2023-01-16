7d7c83630d59e5c11d31ae974cc9860b92f418c9
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-16 21:26:47 +0530

    hook updated

---
+++ web_client/src/utils/hooks/useOnOutSideClick.js
@@ -0,0 +1,42 @@
+/** 
+ *	#########################################################
+ *  										 									
+ *      @author : charanteja379                                 			
+ *      @email  : charanteja379@gmail.com                                  
+ *  	@createedOn : 2023-01-07 19:27:59                               
+ *      @lastModifiedOn : 2023-01-10 12:55:56
+ *  	@desc   : [description]							
+ *  										 								
+ *  #########################################################
+ */
+ 
+
+import { useEffect } from "react";
+
+function useOnOutSideClick(ref, handlerFunc) {
+    useEffect(
+        () => {
+            const listner = (event) => {
+                // Do nothing if clicking ref's element or descendent elements
+                if (!ref.current || ref.current.contains(event.target)) {
+                    // console.log(event.target)
+                    // console.log(ref.current)
+                    return;
+                }
+                // console.log(event.target)
+                // console.log(ref.current)
+                handlerFunc();
+            }
+                
+            document.addEventListener("mousedown", listner);
+            document.addEventListener("touchstart", listner)
+
+            return () => {
+                document.addEventListener("mousedown", listner);
+                document.addEventListener("touchstart", listner)
+            }
+        }, [ref,handlerFunc]
+    )
+}
+
+export default useOnOutSideClick;
