06d484d19f8c9c39ede31afc5dfe857cce99d3a9
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-16 21:25:49 +0530

    movies list updated

--- web_client/src/components/movies/MoviesList.js
+++ web_client/src/components/movies/MoviesList.js
@@ -6,7 +6,7 @@
  * @desc   :desc [description]
  */
 import React, { useContext } from 'react'
-import { ThemeContext } from '../store/contextAPI/themeToggler/ThemeContext';
+import {ThemeContext} from '../../utils/store/contextAPI/themeToggler/ThemeContext'
 
 const MoviesList = () => {
   const {theme} = useContext(ThemeContext)
@@ -25,7 +25,7 @@ const MoviesList = () => {
                 <div className={`movie-cover ${theme}`}>
                   {/* if image is avaliable in local */}
                   {/* <img className='' src='https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt=''></img> */}
-                  <img src={require('../styles/matrix.jpg')}></img>
+                  <img src={require('../../styles/matrix.jpg')}></img>
                   {/* if no image */}
                   {/* <div className={`movie-nocover ${theme}`}><br/><br/><br/><br/><i class="fa-solid fa-triangle-exclamation fa-2x" aria-hidden="true"></i><br/>No Image</div> */}
                 </div>
