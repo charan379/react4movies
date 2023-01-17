abd9279524305074186b9d4f36a69696e8038185
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-18 00:03:14 +0530

    redux store  added

--- web_client/src/index.js
+++ web_client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/react4movies.min.css'
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/react4movies.min.css";
+import { Provider } from "react-redux";
+import store from "./utils/store/reduxStore/Store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
