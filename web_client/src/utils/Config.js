509411b73ba8c26df27b016e9bd4e516b9394354
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-19 18:54:34 +0530

    config added

---
+++ web_client/src/utils/Config.js
@@ -0,0 +1,9 @@
+const TmdbConfig = {
+    tmdbApiKey: process.env.REACT_APP_TMDB_API,
+    tmdbApiUrl : process.env.REACT_APP_TMDB_API_URL,
+    tmdbImagesUrl : process.env.REACT_APP_TMDB_API_IMAGES_URL,
+    tmdbLanguage : process.env.REACT_APP_TMDB_API_LANGUAGE,
+    tmdbRegion : process.env.REACT_APP_TMDB_API_REGION
+}
+
+export {TmdbConfig};
