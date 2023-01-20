a198145580376f24cb312181f9d03b28289b272e
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-20 22:56:35 +0530

    footer updated

--- web_client/src/components/main/Footer.js
+++ web_client/src/components/main/Footer.js
@@ -3,38 +3,24 @@
  *
  *      @author : charanteja379
  *      @email  : charanteja379@gmail.com
- *  	@createedOn : 2023-01-07 19:18:52
- *      @lastModifiedOn : 2023-01-17 15:37:47
- *  	@desc   : [description]
+ *  	  @createedOn : 2023-01-07 19:18:52
+ *      @lastModifiedOn : 2023-01-20 21:53:01
+ *  	  @desc   : [description]
  *
  *  #########################################################
  */
 
-import React, { useCallback, useContext, useRef, useState } from "react";
-import useOnOutSideClick from "../../utils/hooks/useOnOutSideClick";
-import user from "../../static/icons/user.svg";
-import day from "../../static/icons/day.svg";
-import night from "../../static/icons/night.svg";
+import React, {  useContext } from "react";
 import { ThemeContext } from "../../utils/store/contextAPI/themeToggler/ThemeContext";
-import ToogleTheme from "../../utils/store/contextAPI/themeToggler/ToogleTheme";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const { theme } = useContext(ThemeContext);
-
-  //  is dropdown open State
-  const [isDropdwonOpen, setDropdownOpen] = useState(false);
-  const dropdownRef = useRef();
-  useOnOutSideClick(
-    dropdownRef,
-    useCallback(() => {
-      setDropdownOpen(false);
-    }, [])
-  );
   return (
     <>
-      <footer>
-        <p style={{textAlign:"center"}}>MovieBunkers</p>
-      </footer>
+      <div className={`footer ${theme}`}>
+        <Link to="#"><b><u>M</u></b>ovie<b><u>B</u></b>unkers-v</Link><code>2.0</code>
+      </div>
     </>
   );
 };
