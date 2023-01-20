d37c7b1b6e5777375fe3481c69b6897d0896af55
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-20 22:57:58 +0530

    movie list updated

--- web_client/src/components/movies/MoviesList.js
+++ web_client/src/components/movies/MoviesList.js
@@ -1,49 +1,49 @@
-/** 
+/**
  *	#########################################################
- *  										 									
- *      @author : charanteja379                                 			
- *      @email  : charanteja379@gmail.com                                  
- *  	  @createedOn : 2023-01-07 18:43:21                               
- *      @lastModifiedOn : 2023-01-19 18:28:34
- *  	  @desc   : [description]							
- *  										 								
+ *
+ *      @author : charanteja379
+ *      @email  : charanteja379@gmail.com
+ *  	  @createedOn : 2023-01-07 18:43:21
+ *      @lastModifiedOn : 2023-01-20 21:58:55
+ *  	  @desc   : [description]
+ *
  *  #########################################################
  */
- 
-import React, { useContext } from 'react'
-import {ThemeContext} from '../../utils/store/contextAPI/themeToggler/ThemeContext'
-import MovieBox from './MovieBox'
+
+import React, { useContext } from "react";
+import { TmdbConfig } from "../../utils/Config";
+import { ThemeContext } from "../../utils/store/contextAPI/themeToggler/ThemeContext";
+import MovieBox from "./MovieBox";
 
 const MoviesList = (props) => {
-  const {theme} = useContext(ThemeContext)
-  let array = new Array(0,1,2,3,4,5,6,7,8,9,10)
-  console.log(props)
+  const { theme } = useContext(ThemeContext);
+  console.log(props);
   return (
     <>
       {/* Movies List  */}
-      <div className='movies'>
-        {
-          array.map((element, index) => {
-            
-            return (
-              <MovieBox
-                movieData={{
-                  index: index,
-                  link: "",
-                  poster: "",
-                  title: "Matrix",
-                  type: "",
-                  year: 1998,
-                  ratting: 8.03,
-                }}
-              />
-            );
+      <div className={`movies ${theme}`}>
 
-          })
-        }
+        {/* if list source = tmdb */}
+        {props.resultsInfo.source === "tmdb"
+          ? props.resultsArray.map((movie, index) => {
+              return (
+                <MovieBox
+                  movieData={{
+                    index: index,
+                    link: "",
+                    poster: TmdbConfig.tmdbImagesUrl+"w342"+movie.poster_path,
+                    title: (props.resultsInfo.movieType==="movie" ? movie.title : movie.original_name),
+                    type: props.resultsInfo.movieType,
+                    year: (props.resultsInfo.movieType==="movie" ? new Date(movie.release_date).getFullYear() : new Date(movie.first_air_date).getFullYear()),
+                    ratting: movie.vote_average,
+                  }}
+                />
+              );
+            })
+          : null}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default MoviesList;
