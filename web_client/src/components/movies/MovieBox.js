3ecf2cab212dba0f980b0a0a62f718e45a644e8a
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-20 22:57:29 +0530

    movie box updated

--- web_client/src/components/movies/MovieBox.js
+++ web_client/src/components/movies/MovieBox.js
@@ -1,27 +1,54 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../../utils/store/contextAPI/themeToggler/ThemeContext";
+import empty from "../../static/empty.svg";
 
 const MovieBox = (props) => {
   const { theme } = useContext(ThemeContext);
+
+  const handleImageError = (img) => {
+    img.target.onerror = null;
+    img.target.src = empty;
+  };
   return (
     <div key={`box-${props.movieData.index}`} className={`movie-box ${theme}`}>
-      {/* Movie Cover */}
-      <div className={`movie-cover ${theme}`}>
-        {/* if image is avaliable in local */}
-        {/* <img src={`${props.movieData.poster}`} alt={`${props.movieData.title}`}></img> */}
-        <img src={require("../../styles/matrix.jpg")}></img>
-        {/* if no image */}
-        {/* <div className={`movie-nocover ${theme}`}><br/><br/><br/><br/><i class="fa-solid fa-triangle-exclamation fa-2x" aria-hidden="true"></i><br/>No Image</div> */}
+      {/* Movie poster */}
+      <div className={`movie-poster ${theme}`}>
+        <div className="poster-img">
+          <img
+            loading="lazy"
+            onError={handleImageError}
+            src={`${props.movieData.poster}`}
+            alt={`${props.movieData.title}`}
+          ></img>
+        </div>
+        {/* <img src={require("../../styles/matrix.jpg")}></img> */}
+        {/* <img src={require("../../static/empty.svg").default}></img> */}
+        {/* postor backdrop*/}
+        <div className={`movie-poster-backdrop ${theme}`}>
+          <i class="far fa-image fa-2x" aria-hidden="true"></i>
+          <br />
+          No Image
+        </div>
+
+        {/* movie info */}
+        <div className={`movie-info ${theme}`}>
+          {props.movieData.title}
+          <br />
+          {props.movieData.type}
+        </div>
+
+        <div className={`movie-year ${theme}`}>
+          {props.movieData.year ? props.movieData.year : null}
+        </div>
+
+        <div className={`movie-ratting ${theme}`}>
+          {props.movieData.ratting ? props.movieData.ratting : null}
+        </div>
+
+        <div className={`movie-title ${theme}`}>
+          <span>{props.movieData.title}</span>
+        </div>
       </div>
-      <div className={`movie-cover-info ${theme}`}>
-        {props.movieData.title}
-        <br />{" "}
-      </div>
-      <div className={`movie-cover-year ${theme}`}>{props.movieData.year}</div>
-      <div className={`movie-cover-ratting ${theme}`}>
-        {props.movieData.ratting}
-      </div>
-      <div className={`movie-title ${theme}`}>{props.movieData.title}</div>
     </div>
   );
 };
