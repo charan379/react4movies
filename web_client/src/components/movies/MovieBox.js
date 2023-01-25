b2257df78bace74ebbd53ed84b8e3e8f3432e2b4
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-25 15:34:16 +0530

    box id ADDED

--- web_client/src/components/movies/MovieBox.js
+++ web_client/src/components/movies/MovieBox.js
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../../utils/store/contextAPI/themeToggler/ThemeContext";
 import empty from "../../static/empty.svg";
+import { useNavigate } from "react-router-dom";
 
 const MovieBox = (props) => {
   const { theme } = useContext(ThemeContext);
-
+  const navigate = useNavigate();
   const handleImageError = (img) => {
     img.target.onerror = null;
     img.target.src = empty;
   };
+
   return (
-    <div key={`box-${props.movieData.index}`} className={`movie-box ${theme}`}>
+    <div id={`box-${props.movieData.index}`} key={`box-${props.movieData.index}`} className={`movie-box ${theme}`}>
       {/* Movie poster */}
-      <div className={`movie-poster ${theme}`}>
+      <div className={`movie-poster ${theme}`} onClick={()=>navigate(props.movieData.link)}>
         <div className="poster-img">
           <img
             loading="lazy"
@@ -21,11 +23,9 @@ const MovieBox = (props) => {
             alt={`${props.movieData.title}`}
           ></img>
         </div>
-        {/* <img src={require("../../styles/matrix.jpg")}></img> */}
-        {/* <img src={require("../../static/empty.svg").default}></img> */}
         {/* postor backdrop*/}
         <div className={`movie-poster-backdrop ${theme}`}>
-          <i class="far fa-image fa-2x" aria-hidden="true"></i>
+          <i className="far fa-image fa-2x" aria-hidden="true"></i>
           <br />
           No Image
         </div>
