5887b0f6da33a78a94e659b67b6b94a3096e6628
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-25 15:33:53 +0530

    list updated

--- web_client/src/components/movies/MoviesList.js
+++ web_client/src/components/movies/MoviesList.js
@@ -4,7 +4,7 @@
  *      @author : charanteja379
  *      @email  : charanteja379@gmail.com
  *  	  @createedOn : 2023-01-07 18:43:21
- *      @lastModifiedOn : 2023-01-22 16:26:01
+ *      @lastModifiedOn : 2023-01-25 14:52:09
  *  	  @desc   : [description]
  *
  *  #########################################################
@@ -22,7 +22,6 @@ const MoviesList = (props) => {
     <>
       {/* Movies List  */}
       <div className={`movies ${theme}`}>
-
         {/* if list source = tmdb */}
         {props.resultsInfo.source === "tmdb"
           ? props.resultsArray.map((movie, index) => {
@@ -30,11 +29,36 @@ const MoviesList = (props) => {
                 <MovieBox
                   movieData={{
                     index: index,
-                    link: "",
-                    poster: TmdbConfig.tmdbImagesUrl+"w342"+movie.poster_path,
-                    title: (props.resultsInfo.movieType==="movie" ? movie.title : movie.original_name),
+                    link: `/discover/tmdb/${props.resultsInfo.movieType}/${
+                      movie.id
+                    }/${
+                      props.resultsInfo.movieType === "movie"
+                        ? encodeURIComponent(
+                            (
+                              movie.title +
+                              "-" +
+                              new Date(movie.release_date).getFullYear()
+                            ).replace(/[^a-zA-Z0-9]/g, "-")
+                          )
+                        : encodeURIComponent(
+                            (
+                              movie.original_name +
+                              "-" +
+                              new Date(movie.first_air_date).getFullYear()
+                            ).replace(/[^a-zA-Z0-9]/g, "-")
+                          )
+                    }`,
+                    poster:
+                      TmdbConfig.tmdbImagesUrl + "w342" + movie.poster_path,
+                    title:
+                      props.resultsInfo.movieType === "movie"
+                        ? movie.title
+                        : movie.original_name,
                     type: props.resultsInfo.movieType,
-                    year: (props.resultsInfo.movieType==="movie" ? new Date(movie.release_date).getFullYear() : new Date(movie.first_air_date).getFullYear()),
+                    year:
+                      props.resultsInfo.movieType === "movie"
+                        ? new Date(movie.release_date).getFullYear()
+                        : new Date(movie.first_air_date).getFullYear(),
                     ratting: movie.vote_average,
                   }}
                 />
