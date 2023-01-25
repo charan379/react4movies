192cd17e7dc2be51ab132ea6d393dfae03024453
charan379 <37008444+charan379@users.noreply.github.com>
2023-01-25 15:33:30 +0530

    loader added

--- web_client/src/components/movies/tmdb/TmdbMovie.js
+++ web_client/src/components/movies/tmdb/TmdbMovie.js
@@ -1,22 +1,104 @@
-import React, { useContext } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { TmdbConfig } from "../../../utils/Config";
 import { ThemeContext } from "../../../utils/store/contextAPI/themeToggler/ThemeContext";
+import Loader from "../../utils/Loader";
 import MovieDetails from "../MovieDetails";
 import MoviePoster from "../MoviePoster";
 
 function TmdbMovie() {
-  const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
   const params = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [tmdbMovie, setTmdbMovie] = useState({});
+  const fetchMovie = (source) => {
+    setIsLoading(true);
+    axios
+      .get(
+        `${TmdbConfig.tmdbApiUrl}${params.movieType}/${params.tmdbId}?api_key=${TmdbConfig.tmdbApiKey}&append_to_response=videos,images,watch/providers`,
+        {
+          cancelToken: source.token,
+        }
+      )
+      .then((response) => {
+        setTmdbMovie({ ...response.data });
+        setIsLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log(error);
+        } else {
+          console.log(error);
+        }
+      });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const source = axios.CancelToken.source();
+    fetchMovie(source);
+    return () => {
+      source.cancel();
+    };
+  }, [params]);
+
   return (
     <>
       <div className={`movie-page ${theme}`}>
-        <div className="movie-title">The Matrix</div>
-        <div className="movie-poster">
-          <MoviePoster />
-        </div>
-        <div className="movie-details">
-          <MovieDetails />
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <div className="movie-title">
+              {params.movieType === "movie"
+                ? tmdbMovie.title
+                : tmdbMovie.original_name}{" "}
+              &nbsp;{" "}
+              <small>
+                (
+                {params.movieType === "movie"
+                  ? new Date(tmdbMovie.release_date).getFullYear()
+                  : new Date(tmdbMovie.first_air_date).getFullYear()}
+                )
+              </small>
+            </div>
+            <div className="movie-poster">
+              <MoviePoster
+                data={{
+                  url: `${
+                    TmdbConfig.tmdbImagesUrl + "/w500" + tmdbMovie.poster_path
+                  }`,
+                  alt: `${
+                    params.movieType === "movie"
+                      ? tmdbMovie.title
+                      : tmdbMovie.original_name
+                  }`,
+                  tagline: tmdbMovie.tagline,
+                }}
+              />
+            </div>
+            <div className="movie-details">
+              <MovieDetails
+                data={{
+                  genres: tmdbMovie.genres,
+                  title:
+                    params.movieType === "movie"
+                      ? tmdbMovie.title
+                      : tmdbMovie.original_name,
+                  original_title:
+                    params.movieType === "movie"
+                      ? tmdbMovie.original_title
+                      : tmdbMovie.original_name,
+                  original_language: tmdbMovie.original_language,
+                  type: params.movieType,
+                  overview: tmdbMovie.overview,
+                }}
+              />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
