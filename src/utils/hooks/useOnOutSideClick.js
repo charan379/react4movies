6db11e471be0d189cac40047cb8ccb3d3595113d
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-23 00:02:37 +0530

    esc key listner

--- src/utils/hooks/useOnOutSideClick.js
+++ src/utils/hooks/useOnOutSideClick.js
@@ -1,42 +1,50 @@
-/** 
+/**
  *	#########################################################
- *  										 									
- *      @author : charanteja379                                 			
- *      @email  : charanteja379@gmail.com                                  
- *  	@createedOn : 2023-01-07 19:27:59                               
- *      @lastModifiedOn : 2023-01-26 15:44:21
- *  	@desc   : [description]							
- *  										 								
+ *
+ *      @author : charanteja379
+ *      @email  : charanteja379@gmail.com
+ *  	@createedOn : 2023-01-07 19:27:59
+ *      @lastModifiedOn : 2023-02-22 23:54:53
+ *  	@desc   : [description]
+ *
  *  #########################################################
  */
- 
 
 import { useEffect } from "react";
 
 function useOnOutSideClick(ref, handlerFunc) {
-    useEffect(
-        () => {
-            const listner = (event) => {
-                // Do nothing if clicking ref's element or descendent elements
-                if (!ref.current || ref.current.contains(event.target)) {
-                    // console.log(event.target)
-                    // console.log(ref.current)
-                    return;
-                }
-                // console.log(event.target)
-                // console.log(ref.current)
-                handlerFunc();
-            }
-                
-            document.addEventListener("mousedown", listner);
-            document.addEventListener("touchstart", listner)
+  useEffect(() => {
+    const listner = (event) => {
+    //   console.log(ref.current);
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        // console.log(event.target)
+        // console.log(ref.current)
+        if (ref.current) {
+          if (ref.current.contains(event.target) & (event.key === "Escape")) {
+            handlerFunc();
+          }
+        }
+        return;
+      }
+      // console.log(event.target)
+      // console.log(ref.current)
+      handlerFunc();
+    };
 
-            return () => {
-                document.addEventListener("mousedown", listner);
-                document.addEventListener("touchstart", listner)
-            }
-        }, [ref,handlerFunc]
-    )
+    document.addEventListener("mousedown", listner);
+    document.addEventListener("touchstart", listner);
+    document.onkeydown = function (event) {
+      if (event.key === "Escape") {
+        listner(event);
+      }
+    };
+
+    return () => {
+        document.addEventListener("mousedown", listner);
+        document.addEventListener("touchstart", listner);
+    };
+  }, [ref, handlerFunc]);
 }
 
 export default useOnOutSideClick;
