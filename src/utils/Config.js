1fa9e3dedebc85d5cb5ec2a671dc58cdd3b34399
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-06 22:14:09 +0530

    config updated

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,9 +1,12 @@
 const TmdbConfig = {
-    tmdbApiKey: process.env.REACT_APP_TMDB_API,
-    tmdbApiUrl : process.env.REACT_APP_TMDB_API_URL,
-    tmdbImagesUrl : process.env.REACT_APP_TMDB_API_IMAGES_URL,
-    tmdbLanguage : process.env.REACT_APP_TMDB_API_LANGUAGE,
-    tmdbRegion : process.env.REACT_APP_TMDB_API_REGION
-}
+  tmdbApiKey: process.env.REACT_APP_TMDB_API,
+  tmdbApiUrl: process.env.REACT_APP_TMDB_API_URL,
+  tmdbImagesUrl: process.env.REACT_APP_TMDB_API_IMAGES_URL,
+  tmdbLanguage: process.env.REACT_APP_TMDB_API_LANGUAGE,
+  tmdbRegion: process.env.REACT_APP_TMDB_API_REGION,
+};
 
-export {TmdbConfig}
+const Config = {
+  SERVER: process.env.REACT_APP_SERVER,
+};
+export { TmdbConfig, Config };
