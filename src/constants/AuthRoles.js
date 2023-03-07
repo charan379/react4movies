1a749588589ca6ec08255f08a23ef1076260d53c
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-07 23:20:30 +0530

    AuthRoles added

---
+++ src/constants/AuthRoles.js
@@ -0,0 +1,16 @@
+const Roles = Object.freeze({
+  ADMIN: "Admin",
+  MODERATOR: "Moderator",
+  USER: "User",
+  GUEST: "Guest",
+});
+
+export const LevelZero = [...Object.values(Roles)];
+
+export const LevelOne = [Roles.ADMIN, Roles.MODERATOR, Roles.USER];
+
+export const LevelTwo = [Roles.ADMIN, Roles.MODERATOR];
+
+export const LevelThere = [Roles.ADMIN];
+
+export default Roles;
