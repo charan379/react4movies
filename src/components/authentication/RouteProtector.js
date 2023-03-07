a4a58fc3c37c0dc65715aac2bad3cb6d517d229e
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-07 23:22:29 +0530

    RouteProtector implemented

--- src/components/authentication/RouteProtector.js
+++ src/components/authentication/RouteProtector.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import useAuth from "../../utils/hooks/useAuth";
 
-const RouteProtector = () => {
-    
-  return (
-    <div>RouteProtector</div>
-  )
+const RouteProtector = ({allowedRoles}) => {
+  const { auth } = useAuth();
+
+  const location = useLocation();
+
+  if (!auth?.userName) return <Navigate to='/login' state={{form: location}} replace></Navigate>;
+
+  if (allowedRoles.indexOf(auth?.role) !== -1) return <Outlet />;
+
+  if (allowedRoles.indexOf(auth?.role) === -1) return <Navigate to='/un-authorized' state={{form: location}} replace></Navigate>;
+
+};
+
+RouteProtector.defaultProps = {
+  allowedRoles: []
 }
 
-export default RouteProtector
+RouteProtector.propTypes = {
+  allowedRoles: PropTypes.array
+}
+
+export default RouteProtector;
