23be79370851270e5a1d00495fa6c384bb5a3c32
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-07 23:25:47 +0530

    configureStore, persistStore , persistReducer implemented

--- src/utils/store/reduxStore/Store.js
+++ src/utils/store/reduxStore/Store.js
@@ -1,11 +1,22 @@
- 
- import {applyMiddleware , createStore} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { applyMiddleware, createStore } from "redux";
+import {configureStore} from '@reduxjs/toolkit';
+import thunk from "redux-thunk";
+import { persistStore } from 'redux-persist';
+import { composeWithDevTools } from "redux-devtools-extension";
+import persistedReducer from "./reducers";
 
-const store = createStore(
-    rootReducer, composeWithDevTools(applyMiddleware(thunk))
-)
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk],
+    devTools: process.env.NODE_ENV !== 'production',
+})
+
+// const store2 = createStore(
+//     persistedReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+
+export const persistor = persistStore(store);
 
 export default store;
--- src/utils/store/reduxStore/reducers/index.js
+++ src/utils/store/reduxStore/reducers/index.js
@@ -1,10 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import DiscoverReducer from "./DiscoverReducer";
 import UserReducer from "./UserReducer";
+
 
 const rootReducer = combineReducers({
   DiscoverReducer,
   UserReducer,
 });
 
-export default rootReducer;
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
