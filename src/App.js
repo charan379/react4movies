d66f386853bc3f039bc5dbaca834e1f8eced9b8f
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-07 23:21:38 +0530

    auth based routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainComponent from './components/main/MainComponent'
-import Collection from './components/movies/Collection'
-import FetchTitle from './components/movies/FetchTitle'
-import Home from './components/movies/Home'
-import AddTitle from './components/movies/AddTitle'
-import ThemeContextProvider from './utils/store/contextAPI/themeToggler/ThemeContextProvider'
-import PageNotFound from './components/main/PageNotFound'
-import DiscoverTmdb from './components/movies/tmdb/DiscoverTmdb'
-import Loader from './components//utils/Loader'
-import TmdbMovie from './components/movies/tmdb/TmdbMovie'
-import Test from './rough/test'
-import Episode from './components/movies/tmdb/Tv/Episode'
-import Login from './components/authentication/Login'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainComponent from "./components/main/MainComponent";
+import RouteProtector from "./components/authentication/RouteProtector";
+import Collection from "./components/movies/Collection";
+import FetchTitle from "./components/movies/FetchTitle";
+import Home from "./components/movies/Home";
+import AddTitle from "./components/movies/AddTitle";
+import PageNotFound from "./components/main/PageNotFound";
+import DiscoverTmdb from "./components/movies/tmdb/DiscoverTmdb";
+import Loader from "./components//utils/Loader";
+import TmdbMovie from "./components/movies/tmdb/TmdbMovie";
+import Test from "./rough/test";
+import Episode from "./components/movies/tmdb/Tv/Episode";
+import Login from "./components/authentication/Login";
+import Unauthorized from "./components/authentication/Unauthorized";
+import useAuth from "./utils/hooks/useAuth";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Config } from "./utils/Config";
+import Roles, {
+  LevelOne,
+  LevelThere,
+  LevelTwo,
+  LevelZero,
+} from "./constants/AuthRoles";
 
 const App = () => {
 
+
   return (
     <>
-      <ThemeContextProvider>
-        <BrowserRouter>
-          <Routes>
-          <Route path='/' element={<MainComponent/>}>
-            <Route index element = {<Home/>} />
-            <Route path='/collection' element={<Collection/>} />
-            <Route path='/fetch-title' element={<FetchTitle/>} />
-            <Route path='/add-title' element={<AddTitle />} />
-            <Route path='/discover/tmdb' element={<DiscoverTmdb />} />
-            <Route path='/view/tmdb/:titleType/:tmdbId/:title' element={<TmdbMovie />} />
-            <Route path='/loader' element={<Loader />} />
-            <Route path='/test' element={<Test />} />
-            <Route path='/lep' element={<Episode/>} />
-            <Route path='login' element={<Login/>} />
-            <Route path='*' element={<PageNotFound />} />
-          </Route>
-          </Routes>
-        </BrowserRouter>
-      </ThemeContextProvider>
-    </>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainComponent />}>
+            {/* Layout */}
+            <Route index element={<Home />} />
 
-export default App
+            <Route element={<RouteProtector allowedRoles={LevelZero} />}>
+              <Route path="/collection" element={<Collection />} />
+              <Route path="/fetch-title" element={<FetchTitle />} />
+              <Route path="/add-title" element={<AddTitle />} />
+              <Route path="/discover/tmdb" element={<DiscoverTmdb />} />
+
+              <Route element={<RouteProtector allowedRoles={LevelOne} />}>
+                <Route
+                  path="/view/tmdb/:titleType/:tmdbId/:title"
+                  element={<TmdbMovie />}
+                />
+              </Route>
+            </Route>
+
+            <Route path="/loader" element={<Loader />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/lep" element={<Episode />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/un-authorized" element={<Unauthorized />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
