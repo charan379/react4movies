d7a5171bf16c0e58db8e523bc63c90da15fb1d88
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-08 18:44:55 +0530

    useToast Hook

---
+++ src/utils/hooks/useToast.js
@@ -0,0 +1,25 @@
+import { ToastContainer, toast } from "react-toastify";
+import useTheme from "./useTheme";
+
+const useToastify = () => {
+  const {theme} = useTheme();
+
+  const toastContainerOptions = {
+    position:"top-right",
+    autoClose:5000,
+    hideProgressBar:false,
+    newestOnTop:true,
+    closeOnClick: true,
+    rtl:false,
+    pauseOnFocusLoss: true,
+    draggable: true,
+    pauseOnHover: true,
+    theme:theme,
+  }
+  
+
+  return { ToastContainer: ToastContainer,toastContainerOptions: toastContainerOptions,  toast: toast };
+};
+
+
+export default useToastify;
