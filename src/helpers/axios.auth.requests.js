5a1a46dd34d16c2d7b6bd9e6252aa5180ef1882e
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-08 18:44:27 +0530

    auth helpers updated

--- src/helpers/axios.auth.requests.js
+++ src/helpers/axios.auth.requests.js
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Config } from "../utils/Config";
+import MovieBunkersException from "../utils/MovieBunkersException";
 
 const API = Config.SERVER;
 
@@ -7,15 +8,61 @@ export async function fetchWhoAmI(source = axios.CancelToken.source()) {
   try {
     const response = await axios.get(`${API}/auth/who-am-i`, {
       withCredentials: true,
-      cancelToken: source.token
+      cancelToken: source.token,
     });
     return response?.data;
   } catch (error) {
-    const errorResponse = error?.response?.data?.error;
+    const errorResponse = error?.response?.data;
     if (errorResponse) {
-      console.log(JSON.stringify(errorResponse));
+      throw new MovieBunkersException(errorResponse);
     } else {
-      console.log(error);
+      throw new AxiosError(error);
+    }
+  }
+}
+
+export async function authenticateUser(
+  userName,
+  password,
+  source = axios.CancelToken.source()
+) {
+  try {
+    const response = await axios.post(
+      `${API}/auth/cookie-auth`,
+      {
+        userName,
+        password,
+      },
+      {
+        withCredentials: true,
+        cancelToken: source.token,
+      }
+    );
+
+    return response?.data;
+  } catch (error) {
+    const errorResponse = error?.response?.data;
+    if (errorResponse) {
+      throw new MovieBunkersException(errorResponse);
+    } else {
+      throw new AxiosError(error);
+    }
+  }
+}
+
+export async function logout(source = axios.CancelToken.source()) {
+  try {
+    const response = await axios.get(`${API}/auth/logout`, {
+      withCredentials: true,
+      cancelToken: source.token,
+    });
+    return response?.data;
+  } catch (error) {
+    const errorResponse = error?.response?.data;
+    if (errorResponse) {
+      throw new MovieBunkersException(errorResponse);
+    } else {
+      throw new AxiosError(error);
     }
   }
 }
