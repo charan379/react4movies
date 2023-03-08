2be0a9fd5a8b2e31bba56bd379967f37e807c891
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-08 19:07:07 +0530

    minor tweaks

--- src/utils/store/reduxStore/Store.js
+++ src/utils/store/reduxStore/Store.js
@@ -1,8 +1,6 @@
-import { applyMiddleware, createStore } from "redux";
 import {configureStore} from '@reduxjs/toolkit';
 import thunk from "redux-thunk";
 import { persistStore } from 'redux-persist';
-import { composeWithDevTools } from "redux-devtools-extension";
 import persistedReducer from "./reducers";
 
 const store = configureStore({
@@ -10,11 +8,6 @@ const store = configureStore({
     middleware: [thunk],
     devTools: process.env.NODE_ENV !== 'production',
 })
-
-// const store2 = createStore(
-//     persistedReducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// );
 
 
 export const persistor = persistStore(store);
