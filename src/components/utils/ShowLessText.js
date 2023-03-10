033754936be4cc806c078da7d83590083bb2a1e3
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-10 19:18:38 +0530

    `update, bug-fix` style updated, bugs fixed

--- src/components/utils/ShowLessText.js
+++ src/components/utils/ShowLessText.js
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 
-const ShowLessText = ({data}) => {
+const ShowLessText = ({ text, limit }) => {
   const [showMore, setShowMore] = useState(false);
-  const text = data.text;
-  if(text.length <= 150){
+  if (text?.length <= limit ?? 150) {
     return text;
   }
   return (
-    <div>
-        {showMore ? text : `${text.substring(0, 150)}`}
-        <span onClick={() => setShowMore(!showMore)}>
-          {showMore ? " : Show less" : " : Show more"}
-        </span>
-    </div>
+    <>
+      {showMore ? text : `${text.substring(0, limit ?? 150)}`}
+      <span style={
+        {
+          display:"inline",
+          textDecoration: "none",
+          fontStyle: "italic",
+          fontFamily: "monospace",
+          fontWeight: "bold",
+          cursor: "pointer",
+          color:"#4787d6"
+        }
+      } onClick={() => setShowMore(!showMore)}>
+        {showMore ? " ...less" : " ...more"}
+      </span>
+    </>
   );
 };
 
 ShowLessText.defaultProps = {
-    data : {
-        text : "No Data"
-    }
+  data: {
+    text: "No Data"
+  }
 }
 
 export default ShowLessText;
