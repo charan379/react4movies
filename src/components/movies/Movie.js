848fa7dc7e5b15c6ca8208697c5a7d8b5e265736
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-10 18:58:50 +0530

    `new `:Movie component added

---
+++ src/components/movies/Movie.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import useTheme from '../../utils/hooks/useTheme';
+import MovieDetails from './MovieDetails';
+import MoviePoster from './MoviePoster';
+
+const Movie = ({ movie }) => {
+    const { theme } = useTheme();
+    const location = useLocation();
+
+
+    return (
+        <>
+            <div className={`movie-page ${theme}`}>
+
+                <div className="movie-title">
+                    {movie?.title}
+                    <small> ({movie?.year})</small>
+                </div>
+
+                <div className="movie-poster">
+                  <MoviePoster
+                    data={{
+                      url: movie?.poster_path,
+
+                      alt: movie?.title,
+
+                      tagline: movie?.tagline,
+                    }}
+                  />
+                </div>
+
+
+                <div className="movie-details">
+                  <MovieDetails
+                    titleData={{ ...movie }}
+                    titleType={movie.title_type}
+                  />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Movie
