ad83e88720f1efc84fef0038d7c010cfbdd280c8
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-10 19:02:06 +0530

    `update`: MovieModel updated

--- src/components/movies/MovieModal.js
+++ src/components/movies/MovieModal.js
@@ -1,13 +1,13 @@
 import React, { useCallback, useRef } from "react";
 import useOnOutSideClick from "../../utils/hooks/useOnOutSideClick";
 import useTheme from "../../utils/hooks/useTheme";
+import Title from "./Title";
 import TmdbMovie from "./tmdb/TmdbMovie";
 
 const MovieModal = ({ data, open, close }) => {
   const { theme } = useTheme();
   const movieModalRef = useRef();
   const openInNewTab = (url) => {
-    console.log("open bnew tan");
     window.open(url, "_blank", "noreferrer");
   };
 
@@ -18,24 +18,25 @@ const MovieModal = ({ data, open, close }) => {
     <>
       <div ref={movieModalRef} className={`movie-overlay ${theme}`}>
         <div className="movie-modalContainer">
-          <div className="open-newTab" onClick={() => openInNewTab(data.link)}>
+          {/* <div className="open-newTab" onClick={() => openInNewTab(data.link)}>
             <i className="fas fa-external-link-alt fa-2x"></i>
-          </div>
+          </div> */}
           <div onClick={close} className="closeBtn">
             <i className="fas fa-times fa-lg"></i>
           </div>
 
           <div  className="">
-            {data.source === "tmdb" ? (
+            <Title id={data?.id} titleSource={data?.source} titleType={data?.titleType}></Title>
+            {/* {data.source === "tmdb" ? (
               <TmdbMovie
                 movieData={{
                   tmdb_id: data.tmdb_id,
                   tittle: data.tittle,
                   year: data.year,
-                  titleType: data.titleType,
+                  titleType: data.title_type,
                 }}
               />
-            ) : null}
+            ) : null} */}
           </div>
         </div>
       </div>
