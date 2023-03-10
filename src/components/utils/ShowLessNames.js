8789e0105850818cf597c6619f83fcfc18162901
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-10 19:17:37 +0530

    `new` ShowLessNames comp created

---
+++ src/components/utils/ShowLessNames.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const ShowLessNames = ({ commaSepratedText, limit }) => {
+  const [showMore, setShowMore] = useState(false);
+  if (commaSepratedText?.split(",")?.length <= limit ?? 3) {
+    return commaSepratedText;
+  }
+  return (
+    <>
+      {showMore ? commaSepratedText : `${commaSepratedText?.split(",").slice(0, limit ?? 3).map(ele => ele).join(",")}`}
+      <span style={
+        {
+          display:"inline",
+          textDecoration: "none",
+          fontStyle: "italic",
+          fontFamily: "monospace",
+          fontWeight: "bold",
+          cursor: "pointer",
+          color:"#4787d6"
+        }
+      } onClick={() => setShowMore(!showMore)}>
+        {showMore ? " ...less" : " ...more"}
+      </span>
+    </>
+  );
+};
+
+ShowLessNames.defaultProps = {
+  data: {
+    text: "No Data"
+  }
+}
+
+export default ShowLessNames;
