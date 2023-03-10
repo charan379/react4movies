7de6d3247ee0ba4338dba4be4d8c9775fbdde83b
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-10 23:54:14 +0530

    `new` fetchTmdbTvSeason created

--- src/helpers/tmdb.requests.js
+++ src/helpers/tmdb.requests.js
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { Config } from "../utils/Config";
 import MovieBunkersException from "../utils/MovieBunkersException";
 
@@ -8,7 +8,7 @@ const API = Config.TMDB_API;
 
 export async function searchTmdb(query, source = axios.CancelToken.source()) {
     try {
-        const response = await axios.get(`${API}/search`,{ params: { ...query } },{ CancelToken: source.token });
+        const response = await axios.get(`${API}/search`, { params: { ...query } }, { CancelToken: source.token });
         return response?.data;
     } catch (error) {
         const errorResponse = error?.response?.data;
@@ -23,7 +23,22 @@ export async function searchTmdb(query, source = axios.CancelToken.source()) {
 
 export async function fetchTmdbTitle({ id, titleType }, source = axios.CancelToken.source()) {
     try {
-        const response = await axios.get(`${API}/${titleType}/${id}`,{ CancelToken: source.token });
+        const response = await axios.get(`${API}/${titleType}/${id}`, { CancelToken: source.token });
+        return response?.data;
+    } catch (error) {
+        const errorResponse = error?.response?.data;
+        if (errorResponse) {
+            throw new MovieBunkersException(errorResponse);
+        } else {
+            throw error;
+        }
+
+    }
+}
+
+export async function fetchTmdbTvSeason({ tmdbShowId, seasonNumber }, source = axios.CancelToken.source()) {
+    try {
+        const response = await axios.get(`${API}/tv/${tmdbShowId}/season/${seasonNumber}`, { CancelToken: source.token });
         return response?.data;
     } catch (error) {
         const errorResponse = error?.response?.data;
