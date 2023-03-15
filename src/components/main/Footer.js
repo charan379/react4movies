1c719bb5c88e978ae6293f1f2dc8bceb83cd44a9
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-15 20:06:48 +0530

    `update` tmdb attribution

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import useTheme from "../../utils/hooks/useTheme";
+import tmdbLogo from "../../static/tmdb-logo.svg"
 
 const Footer = () => {
   const { theme } = useTheme();
   return (
     <>
       <div className={`footer ${theme}`}>
-        <Link to="#" tabIndex="-1"><b><u>M</u></b>ovie<b><u>B</u></b>unkers-v</Link><code>2.0</code>
+        <Link to="#" tabIndex="-1"><span>react<b><u>4</u></b>movies<sup>v<code>2.0</code></sup></span></Link>
+        <Link to="#" tabIndex="-1"><span> <b><u>M</u></b>ovie<b><u>B</u></b>unkers<sup>v<code>2.0</code></sup></span></Link>
+        <Link to='#' tabIndex="-1" className="tmdb-logo"><img src={tmdbLogo}></img></Link>
       </div>
     </>
   );
