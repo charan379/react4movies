7c4d60962abbdcc0fd381f28746e8194e7762c60
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-26 12:09:21 +0530

    `update` reset filters on CTRL+D

--- src/utils/hooks/useCollectionSearch.js
+++ src/utils/hooks/useCollectionSearch.js
@@ -1,17 +1,24 @@
-import { useDispatch, useSelector } from "react-redux"
-import { updateCollectionSearch } from "../store/reduxStore/actions/CollectionSearchActions";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  resetCollectionSearch,
+  updateCollectionSearch,
+} from "../store/reduxStore/actions/CollectionSearchActions";
 
 const useCollectionSearch = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const query = useSelector((state) => state.CollectionSearchReducer);
 
-    const query = useSelector((state) => state.CollectionSearchReducer);
+  const setQuery = (queryObject) =>
+    dispatch(updateCollectionSearch(queryObject));
 
-    const setQuery = (queryObject) => dispatch(updateCollectionSearch(queryObject))
+  const resetQuery = () => dispatch(resetCollectionSearch());
 
-    return { collectionQuery: query, setCollectionQuery: setQuery }
-}
+  return {
+    collectionQuery: query,
+    setCollectionQuery: setQuery,
+    resetCollectionSearch: resetQuery,
+  };
+};
 
 export default useCollectionSearch;
--- src/utils/hooks/useTmdbSearch.js
+++ src/utils/hooks/useTmdbSearch.js
@@ -1,17 +1,23 @@
-import { useDispatch, useSelector } from "react-redux"
-import { updateTmdbSearch } from "../store/reduxStore/actions/TmdbSearchActions";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  resetTmdbSearch,
+  updateTmdbSearch,
+} from "../store/reduxStore/actions/TmdbSearchActions";
 
 const useTmdbSearch = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const query = useSelector((state) => state.TmdbSearchReducer);
 
-    const query = useSelector((state) => state.TmdbSearchReducer);
+  const setQuery = (queryObject) => dispatch(updateTmdbSearch(queryObject));
 
-    const setQuery = (queryObject) => dispatch(updateTmdbSearch(queryObject))
+  const resetQuery = () => dispatch(resetTmdbSearch());
 
-    return { tmdbSearch: query, setTmdbSearch: setQuery }
-}
+  return {
+    tmdbSearch: query,
+    setTmdbSearch: setQuery,
+    restTmdbSearch: resetQuery,
+  };
+};
 
 export default useTmdbSearch;
