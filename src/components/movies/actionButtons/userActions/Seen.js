46f21c4bb6ade98e472e0677b33993d8068d067c
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-28 17:33:00 +0530

    `new` spinners on action buttons

--- src/components/movies/actionButtons/userActions/Seen.js
+++ src/components/movies/actionButtons/userActions/Seen.js
@@ -1,77 +1,85 @@
-import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Config } from '../../../../utils/Config';
+import useMovieBunkersAPI from '../../../../utils/hooks/useMovieBunkersAPI';
 import useTitle from '../../../../utils/hooks/useTitle';
 
 const Seen = ({ toast }) => {
 
-    const API = Config.MOVIEBUNKERS_API;
-
     const { title, setTitle } = useTitle();
 
-    const addToSeenTitles = (base64TitleId) => {
-        // const toastId = toast.loading("Adding to seen...", { position: "top-right", closeButton: true, autoClose: 1000 });
-        axios.post(`${API}/userdata/add-to-seen/${base64TitleId}`, {}, { withCredentials: true })
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { movieBunkersAPI } = useMovieBunkersAPI();
+
+    const addToSeenTitles = (event, base64TitleId) => {
+        event.preventDefault();
+        setIsLoading(true);
+        movieBunkersAPI.post(`/userdata/add-to-seen/${base64TitleId}`)
             .then((response) => {
-                // toast.update(toastId, {
-                //     render: response.data?.message,
-                //     type: "success",
-                //     isLoading: false,
-                //     autoClose: 2000,
-                //     delay: 50,
-                // });
                 setTitle({ ...title, seenByUser: true, unseenByUser: false })
-            }).catch((error) => {
+                // toast.success(response?.data?.message, { autoClose: 1000, position: "top-left", closeButton: true, delay: 50 })
+            })
+            .catch((error) => {
                 const errMsg = error?.response?.data?.error?.message
-                toast.error(errMsg ?? "Somthing went wrong", { autoClose: 3000, position: "top-right", closeButton: true })
+                toast.error(errMsg ?? "Somthing went wrong", { autoClose: 2000, position: "top-right", closeButton: true })
+            }).finally(() => {
+                setIsLoading(false);
             })
     }
 
-    const addToUnseenTitles = (base64TitleId) => {
-        // const toastId = toast.loading("Adding to unseen...", { position: "top-right", closeButton: true, autoClose: 1000 });
-        axios.post(`${API}/userdata/add-to-unseen/${base64TitleId}`, {}, { withCredentials: true })
+    const addToUnseenTitles = (event, base64TitleId) => {
+        event.preventDefault();
+        setIsLoading(true);
+        movieBunkersAPI.post(`/userdata/add-to-unseen/${base64TitleId}`)
             .then((response) => {
-                // toast.update(toastId, {
-                //     render: response.data?.message,
-                //     type: "success",
-                //     isLoading: false,
-                //     autoClose: 2000,
-                //     delay: 50,
-                // });
                 setTitle({ ...title, unseenByUser: true, seenByUser: false })
-            }).catch((error) => {
+                // toast.success(response?.data?.message, { autoClose: 1000, position: "top-left", closeButton: true, delay: 50 })
+            })
+            .catch((error) => {
                 const errMsg = error?.response?.data?.error?.message
-                toast.error(errMsg ?? "Somthing went wrong", { autoClose: 3000, position: "top-right", closeButton: true })
+                toast.error(errMsg ?? "Somthing went wrong", { autoClose: 2000, position: "top-right", closeButton: true })
+            }).finally(() => {
+                setIsLoading(false);
             })
     }
-
-
 
     return (
 
         <>
             {title?.seenByUser && (
-                <Link className="action-button" onClick={() => addToUnseenTitles(btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
+                <Link className="action-button" onClick={(event) => addToUnseenTitles(event, btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
                     <span /*style={{ color: 'rgba(16,125,172, 1)' }} */>
-                        <i className="fas fa-eye fa-lg"></i>
+
+                        {isLoading
+                            ? <i class="fas fa-circle-notch fa-pulse fa-lg"></i>
+                            : <i className="fas fa-eye fa-lg"></i>
+                        }
                     </span>
                 </Link>
             )}
 
             {title?.unseenByUser && (
-                <Link className="action-button" onClick={() => addToSeenTitles(btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
+                <Link className="action-button" onClick={(event) => addToSeenTitles(event, btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
                     <span /*style={{ color: 'rgba(16,125,172, 1)' }} */>
-                        <i className="fas fa-eye-slash fa-lg"></i>
+                        {isLoading
+                            ? <i class="fas fa-circle-notch fa-pulse fa-lg"></i>
+                            : <i className="fas fa-eye-slash fa-lg"></i>
+                        }
                     </span>
                 </Link>
             )}
 
             {!title?.unseenByUser && !title?.seenByUser && (
-                <Link className="action-button" onClick={() => addToSeenTitles(btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
-                    <span style={{ opacity: '0.3' }}>
-                        <i className="fas fa-eye fa-lg"></i>
-                    </span>
+                <Link className="action-button" onClick={(event) => addToSeenTitles(event, btoa(title?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_'))}>
+
+                    {isLoading
+                        ? (<span>
+                            <i class="fas fa-circle-notch fa-pulse fa-lg"></i>
+                        </span>)
+                        : (<span style={{ opacity: '0.3' }}>
+                            <i className="fas fa-eye fa-lg"></i>
+                        </span>)
+                    }
                 </Link>
             )}
         </>
