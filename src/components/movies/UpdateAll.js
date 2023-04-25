08ccfafa68b0d26f7ad3f6aeecf49817c883e00e
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-25 23:58:18 +0530

    `udpate` updateAll titles component updated

--- src/components/movies/UpdateAll.js
+++ src/components/movies/UpdateAll.js
@@ -9,63 +9,148 @@ import { Link } from 'react-router-dom';
 import useSeasonsUpdater from '../../utils/hooks/useSeasonsUpdater';
 
 const UpdateAll = () => {
+    // Load theme from custom hook
     const { theme } = useTheme();
+
+    // State for keeping track of progress
     const [state, setState] = useState(0);
+
+    // State for keeping track of the currently updating movie
     const [title, setTitle] = useState({});
+
+    // Load API hooks
     const { movieBunkersAPI } = useMovieBunkersAPI();
     const { tmdbAPI } = useTmdbAPI();
+
+    // Load custom hook for updating TV show seasons
     const { updateSeasons } = useSeasonsUpdater();
+
+    // State for keeping track of movies
     const [movies, setMovies] = useState([]);
+
+    // State for keeping track of total number of movies
     const [totalMovies, setTotalMovies] = useState(0);
+
+    // State for keeping track of current index
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    // State for keeping track of start time
     const [startTime, setStartTime] = useState(null);
+
+    // State for keeping track of end time
     const [endTime, setEndTime] = useState(null);
+
+    // State for keeping track of updated movies
     const [updated, setUpdated] = useState([]);
+
+    // State for keeping track of errors
     const [errors, setErrors] = useState([]);
 
     useEffect(() => {
-        movieBunkersAPI.get(`/titles?search=&genre=&movie=1&starred=0&favourite=0&seen=0&age.gte=0&country=IN&page=1&minimal=true&tv=1&sort_by=createdAt.desc&&age.lte=26&pageNo=1`)
-            .then((result) => {
-                setMovies(result?.data?.list);
-                setTotalMovies(result?.data?.list?.length ?? 0)
-            })
-            .catch(err => {
-                setErrors((errors) => [...errors, { name: 'get all movies', error: err?.message }])
-            })
+        // Define an asynchronous function to fetch movies from the API
+        const fetchMovies = async () => {
+            try {
+                // Make a GET request to the '/titles' endpoint with query params
+                const response = await movieBunkersAPI.get('/titles', {
+                    params: {
+                        search: '',
+                        genre: '',
+                        movie: 1,
+                        tv: 1,
+                        'age.gte': 0,
+                        'age.lte': 26,
+                        country: 'IN',
+                        starred: 0,
+                        favourite: 0,
+                        seen: 0,
+                        minimal: true,
+                        'sort_by': 'createdAt.desc',
+                        pageNo: 1
+                    }
+                });
+
+                // Set the state with the list of movies received from the API
+                setMovies(response?.data?.list);
+                // Set the total number of movies received from the API
+                setTotalMovies(response?.data?.list?.length ?? 0);
+            } catch (error) {
+                // If there's an error, add it to the list of errors in the state
+                setErrors((errors) => [
+                    ...errors,
+                    { name: 'get all movies', error: error?.message },
+                ]);
+            }
+        };
+
+        // Call the function to fetch movies
+        fetchMovies();
     }, []);
 
+
+    /**
+     * Updates the details of movies in the MovieBunkers API using the TMDB API
+     * @returns {Promise<void>}
+     */
     const updateMovies = async () => {
+        // Start tracking the time it takes to update all movies
         setStartTime(Date.now());
+
+        // Loop through all movies
         for (let i = 0; i < movies.length; i++) {
             const movie = movies[i];
             try {
+                // Update the current movie index
                 setCurrentIndex(i + 1);
+
+                // Set the current movie as the title to be updated
                 setTitle({ ...movie });
+
+                // Get updated movie details from TMDB API
                 const res = await tmdbAPI.get(`${movie?.title_type}/${movie?.tmdb_id}`);
                 const update = res?.data;
-                const { data: { title: updatedTitle } } = await movieBunkersAPI.put(`/titles/update/id/${btoa(movie?._id).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')}`, update);
+
+                // Update movie details in MovieBunkers API
+                const {
+                    data: { title: updatedTitle },
+                } = await movieBunkersAPI.put(
+                    `/titles/update/id/${btoa(movie?._id)
+                        .replace(/=/g, '')
+                        .replace(/\+/g, '-')
+                        .replace(/\//g, '_')}`,
+                    update
+                );
+
+                // If the updated title is a TV show, update its seasons
                 if (updatedTitle?.title_type === 'tv') {
                     await updateSeasons({ tmdbTvId: updatedTitle?.tmdb_id, moviebunkersTitleId: updatedTitle?._id, numberOfSeasons: updatedTitle?.number_of_seasons })
                 }
+
+                // Update progress state and log the updated movie
                 setState(Math.floor(((i + 1) / movies.length) * 100));
                 setUpdated((updated) => [...updated, { name: `${movie?.title} : ${movie?.tmdb_id}`, status: 'Updated' }])
             } catch (error) {
+                // Log the failed update
                 setUpdated((updated) => [...updated, { name: `${movie?.title} : ${movie?.tmdb_id}`, status: 'Failed', error: error?.message }])
                 setErrors((errors) => [...errors, { name: `${movie?.title} : ${movie?.tmdb_id}`, error: error?.message }])
             }
         }
+
+        // End tracking the time it takes to update all movies
         setEndTime(Date.now());
     };
 
+    // Calculate elapsed time of updateMovies funtion if both start and end times are available
     const elapsedTime = startTime && endTime ? endTime - startTime : null;
 
     return (
         <div className={`collection-sync ${theme}`}>
+            {/* Sync All Movies heading */}
             <h3 className="heading">Update/Sync All</h3>
 
-            {/* progress bar */}
+            {/* Progress bar */}
             <div className='progress-bar-container'>
                 <ProgressBar bgcolor="#32C104" progress={state} height={'20px'} />
+                {/* Progress info */}
                 {title?.title && state < 100 && (
                     <>
                         <div className='progress-info'>
@@ -79,35 +164,43 @@ const UpdateAll = () => {
                             </span>
                         </div>
                     </>
-
                 )}
             </div>
 
+            {/* Start Update button */}
             {currentIndex === 0 && (
                 <button className='button' onClick={updateMovies}>Start Update</button>
             )}
 
+            {/* All Movies Updated message */}
             {state === 100 && (
                 <>
                     <p><i style={{ color: '#32C104' }} className="fas fa-check-circle fa-lg"></i> &nbsp; All Movies Updated</p>
+                    {/* Elapsed time */}
                     <p><i style={{ color: '#7A50EE' }} className="fas fa-hourglass-end"></i> &nbsp; Elapsed time: {formatTime(elapsedTime)}</p>
-                    <p><Link className='button' to={'/'}>
-                        Home
-                    </Link></p>
+                    {/* Home button */}
+                    <p>
+                        <Link className='button' to={'/'}>
+                            Home
+                        </Link>
+                    </p>
                 </>
-
             )}
 
+            {/* Updating message */}
             {state < 100 && currentIndex > 0 && (
                 <p> <i className="fas fa-sync-alt fa-pulse fa-lg"></i> &nbsp; Updating sit back and relax...</p>
             )}
 
+            {/* Updated Movies logs */}
             <Logger logs={updated} title="Updated Movies" key={1} />
             <br />
-            <Logger logs={errors} title="Errors" key={2} />
 
+            {/* Errors logs */}
+            <Logger logs={errors} title="Errors" key={2} />
         </div>
     );
+
 };
 
 export default UpdateAll;
