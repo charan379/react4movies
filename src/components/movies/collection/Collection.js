a80a4b4690d6b1ec22976abf9bf6082387789d78
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-26 16:13:33 +0530

    `update` codecleaning

--- src/components/movies/collection/Collection.js
+++ src/components/movies/collection/Collection.js
@@ -3,8 +3,11 @@ import MoviesList from './MoviesList'
 import useTheme from '../../../utils/hooks/useTheme'
 
 const Collection = () => {
-    const {theme} = useTheme();
+    // Get the current theme from the useTheme hook
+    const { theme } = useTheme();
+
     return (
+        // Render a div that wraps the MoviesList component
         <React.Fragment>
             <div className="row">
                 <div className={`col-md-12 collection-wrapper ${theme}`}>
@@ -17,4 +20,4 @@ const Collection = () => {
     )
 }
 
-export default Collection
+export default Collection;
