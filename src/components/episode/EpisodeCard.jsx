c8dd21dc67f16a4b5e18f4a4c6d24c81d511e3da
charan379 <37008444+charan379@users.noreply.github.com>
2023-05-13 17:05:34 +0530

    removed yt-player for episode-card and added loader, onClick funtion to more button

--- src/components/episode/EpisodeCard.jsx
+++ src/components/episode/EpisodeCard.jsx
@@ -1,57 +1,85 @@
-import './episode-card.style.css';
-import { useTheme } from 'hooks'
-import React from 'react'
-import { ShowLessText } from 'components/common';
-import { convertIsoDate } from 'utils';
-import { EpisodePoster } from './EpisodePoster';
-import { PlayTrailer } from 'features/title-actions/PlayTrailer';
+import "./episode-card.style.css";
+import { useTheme } from "hooks";
+import React from "react";
+import { ShowLessText } from "components/common";
+import { convertIsoDate } from "utils";
+import { EpisodePoster } from "./EpisodePoster";
 
-const EpisodeCard = ({ episode, latest = false, upcoming = false, moreButton = false }) => {
-    const { theme } = useTheme();
+const EpisodeCard = ({
+  episode,
+  latest = false,
+  upcoming = false,
+  moreButton = false,
+  isLoading,
+  onClick,
+}) => {
+  const { theme } = useTheme();
 
-    if (!episode && moreButton) {
-        return (
-            <div className={`global-last-card  ${theme}`} title='View more episodes'>
-                <div className='more-button-section'>
-                    <div className='more-button'>
-                        <span>
-                            <i class="fas fa-angle-double-right"></i>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
+  if (!episode && moreButton) {
     return (
-        <>
-            <div className={`episode-card ${theme}`}>
-                <div className='poster-section'>
-                    <EpisodePoster still_path={episode?.still_path} episode_name={episode?.name} />
-                </div>
-                <div className='details-section'>
-                    <div className='action-buttons'>
-                        <PlayTrailer videos={episode?.videos} className={`action-button`} />
-                    </div>
-                    <div className='episode-details'>
-                        {latest && <h5 className='sub-heading'>Latest</h5>}
-                        {upcoming && <h5 className='sub-heading'>Upcoming</h5>}
-                        <h4 className='sub-heading'>{episode?.name}</h4>
-                        <h5 className='link'>Season {episode?.season_number} | Episode {episode?.episode_number}</h5>
-                        {episode?.runtime && <span><i class="fas fa-clock"></i> {episode?.runtime} mins</span>}
-                        {episode?.air_date && (<span><i class="far fa-calendar-alt"></i> {convertIsoDate(episode?.air_date)}</span>)}
+      <div
+        className={`global-last-card  ${theme}`}
+        title="View more episodes"
+        onClick={onClick}
+      >
+        <div className="more-button-section" style={{ cursor: "pointer" }}>
+          <div className="more-button">
+            <span>
+              {isLoading ? (
+                <i class="fas fa-circle-notch fa-pulse"></i>
+              ) : (
+                <i class="fas fa-angle-double-right"></i>
+              )}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-                        {episode?.overview && (
-                            <div className='episode-overview'>
-                                <ShowLessText text={episode?.overview} limit={150} />
-                            </div>
-                        )}
+  return (
+    <>
+      <div className={`episode-card ${theme}`}>
+        <div className="poster-section">
+          <EpisodePoster
+            still_path={episode?.still_path}
+            episode_name={episode?.name}
+          />
+        </div>
+        <div className="details-section">
+          {/* <div className="action-buttons">
+            <PlayTrailer videos={episode?.videos} className={`action-button`} />
+          </div> */}
+          <div className="episode-details">
+            {latest && <h5 className="sub-heading">Latest</h5>}
+            {upcoming && <h5 className="sub-heading">Upcoming</h5>}
+            <h4 className="sub-heading">{episode?.name}</h4>
+            <h5 className="link">
+              Season {episode?.season_number} | Episode{" "}
+              {episode?.episode_number}
+            </h5>
+            {episode?.runtime && (
+              <span>
+                <i class="fas fa-clock"></i> {episode?.runtime} mins
+              </span>
+            )}
+            {episode?.air_date && (
+              <span>
+                <i class="far fa-calendar-alt"></i>{" "}
+                {convertIsoDate(episode?.air_date)}
+              </span>
+            )}
 
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+            {episode?.overview && (
+              <div className="episode-overview">
+                <ShowLessText text={episode?.overview} limit={150} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export { EpisodeCard }
+export { EpisodeCard };
