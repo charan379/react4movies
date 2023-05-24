149cdd0de6590af75b4f898348df201f29ca46d5
charan379 <37008444+charan379@users.noreply.github.com>
2023-05-24 21:35:15 +0530

    test

--- src/app/titles/[titleId]/page.jsx
+++ src/app/titles/[titleId]/page.jsx
@@ -1,22 +1,22 @@
 import axios from "axios";
 import React from "react";
 
-// export async function generateMetadata({ params: { titleId } }) {
-//   const { data } = await axios.get(
-//     `https://oxoziko43a.execute-api.ap-southeast-1.amazonaws.com/dev/tmdb/movie/${titleId}`
-//   );
+export async function generateMetadata({ params: { titleId } }) {
+  const { data } = await axios.get(
+    `https://oxoziko43a.execute-api.ap-southeast-1.amazonaws.com/dev/tmdb/movie/${titleId}`
+  );
 
-//   return {
-//     title: data?.title,
-//     description: data?.overview,
-//   };
-// }
+  return {
+    title: data?.title,
+    description: data?.overview,
+  };
+}
 
 export default async function Title({ params: { titleId } }) {
-//   const { data } = await axios.get(
-//     `https://oxoziko43a.execute-api.ap-southeast-1.amazonaws.com/dev/tmdb/movie/${titleId}`
-//   );
+  const { data } = await axios.get(
+    `https://oxoziko43a.execute-api.ap-southeast-1.amazonaws.com/dev/tmdb/movie/${titleId}`
+  );
 
-//   console.log(data);
+  console.log(data);
   return <div>Title : {titleId}</div>;
 }
