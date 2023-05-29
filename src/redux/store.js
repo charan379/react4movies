52e92b6fcbc1b932b8173107cedd5c92de4f34d0
charan379 <37008444+charan379@users.noreply.github.com>
2023-05-29 13:23:59 +0530

    disable redux devtools on production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -57,6 +57,7 @@ export const store = configureStore({
                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             },
         }),
+    devTools: process.env.NODE_ENV !== 'production',
 });
 
 export const persistor = persistStore(store);
