efa3dd762bf76e79280fb806c6d6eb3199c5219e
charan379 <37008444+charan379@users.noreply.github.com>
2023-05-30 17:56:56 +0530

    remove unstable config

--- next.config.js
+++ next.config.js
@@ -13,19 +13,6 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "connect-src 'self' vitals.vercel-insights.com localhost:3001 moviebunkers-api.onrender.com oxoziko43a.execute-api.ap-southeast-1.amazonaws.com"
-                    }
-                ]
-            }
-        ];
-    },
 }
 
 module.exports = nextConfig
