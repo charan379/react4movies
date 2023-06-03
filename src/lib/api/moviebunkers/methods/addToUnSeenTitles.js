94d00a1ff39caf1f4a6edd6ad8dc1a3455e8c9b4
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-03 18:46:29 +0530

    mark title as seen or unseen

---
+++ src/lib/api/moviebunkers/methods/addToUnSeenTitles.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { moviebunkersAPI } from "..";
+
+export async function addToUnSeenTitles({ mbdbTitleId, source = { token: null } }) {
+    try {
+        // Make the API request to add title user unseen list
+        const res = await moviebunkersAPI().post(`/userdata/add-to-unseen/${mbdbTitleId}`, {
+            cancelToken: source.token
+        })
+
+        if (res?.data) {
+            return res.data;
+        } else {
+            throw new Error('Error adding to unseen list')
+        }
+
+    } catch (error) {
+        // Get error message from response, or use a generic message.
+        let message = '';
+
+        // Handle errors by returning the appropriate message
+        if (axios.isCancel(error)) {
+            message = 'cancelled'
+        } else {
+            message = error?.response?.data?.error?.message ??
+                error?.message ??
+                "Something went wrong";
+        }
+
+        throw new Error(message);
+    }
+}
