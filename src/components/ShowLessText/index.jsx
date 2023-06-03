ce5864aefae339c2a185dc90e36e42caa115436c
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-03 14:16:14 +0530

    show less text

---
+++ src/components/ShowLessText/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const ShowLessText = ({ text, limit }) => {
+  const [showMore, setShowMore] = useState(false);
+  if (!text) {
+    return "";
+  }
+  if (text?.length <= limit ?? 150) {
+    return text;
+  }
+  return (
+    <>
+      {showMore ? text : `${text.substring(0, limit ?? 150)}`}
+      <span
+        style={{
+          display: "inline",
+          textDecoration: "none",
+          fontStyle: "italic",
+          fontFamily: "monospace",
+          fontWeight: "bold",
+          cursor: "pointer",
+          color: "#4787d6",
+        }}
+      >
+        <button
+          style={{
+            display: "inline",
+            textDecoration: "none",
+            fontStyle: "italic",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            cursor: "pointer",
+            color: "#4787d6",
+            borderWidth: "0px",
+          }}
+          title={showMore ? "Show less" : "Show more"}
+          onClick={(event) => {
+            event.preventDefault();
+            setShowMore(!showMore);
+          }}
+        >
+          {showMore ? " ...less" : " ...more"}
+        </button>
+      </span>
+    </>
+  );
+};
+
+export default ShowLessText;
