68751184e999c35143da08e43d87c80f0795aff4
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-03 18:47:11 +0530

    add / remove title from starred

---
+++ src/components/TitleActions/Star/index.jsx
@@ -0,0 +1,134 @@
+import styles from "../TitleActions.module.css";
+import React, { useState } from "react";
+// font awesome library
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { addToStarredTitles } from "@/lib/api/moviebunkers/methods/addToStarredTitles";
+import { removeFromStarredTitles } from "@/lib/api/moviebunkers/methods/removeFromStarredTitles";
+library.add(fas, far, fab);
+
+const Star = ({
+  toast,
+  className = styles.actionButton,
+  titleId = null,
+  starredByUser = false,
+  auth,
+}) => {
+  const [isStarred, setStarred] = useState(starredByUser);
+
+  // State to keep track of whether the button is in a loading state or not
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Function to add a title to the user's starred titles
+  const handleAddToStarred = async (event, base64TitleId) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // Send a request to the API to add the title to the user's starred titles
+      await addToStarredTitles({ mbdbTitleId: base64TitleId, auth });
+      // Update the state to reflect that the title is now starred by the user
+      setStarred(true);
+    } catch (error) {
+      // If there was an error, display an error toast message
+      const errMsg = error?.message;
+      toast.error(errMsg ?? "Somthing went wrong", {
+        autoClose: 2000,
+        position: "top-right",
+        closeButton: true,
+      });
+    } finally {
+      // Set the loading state back to false
+      setIsLoading(false);
+    }
+  };
+
+  // Function to remove a title from the user's starred titles
+  const handleRemoveFromStarred = async (event, base64TitleId) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // Send a request to the API to remove the title from the user's starred titles
+      await removeFromStarredTitles({ mbdbTitleId: base64TitleId, auth });
+      // Update the state to reflect that the title is no longer starred by the user
+      setStarred(false);
+    } catch (error) {
+      // If there was an error, display an error toast message
+      const errMsg = error?.response?.data?.error?.message;
+      toast.error(errMsg ?? "Somthing went wrong", {
+        autoClose: 2000,
+        position: "top-right",
+        closeButton: true,
+      });
+    } finally {
+      // Set the loading state back to false
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {/* If the title is already starred by the user, display a button to remove it from their starred titles */}
+      {isStarred && (
+        <button
+          className={className}
+          onClick={(event) =>
+            handleRemoveFromStarred(
+              event,
+              btoa(titleId)
+                .replace(/=/g, "")
+                .replace(/\+/g, "-")
+                .replace(/\//g, "_")
+            )
+          }
+          data-tooltip={`Remove from starred`}
+          data-flow="up"
+        >
+          {/* Display a loading spinner if the button is in a loading state */}
+          {isLoading ? (
+            <span>
+              <FontAwesomeIcon icon={["fas", "circle-notch"]} size="lg" pulse />
+            </span>
+          ) : (
+            <span style={{ color: "rgb(255 149 0)" }}>
+              <FontAwesomeIcon icon={["fas", "star"]} size="lg" />
+            </span>
+          )}
+        </button>
+      )}
+
+      {/* Display a button to add this title to  starred titles */}
+      {!isStarred && (
+        <button
+          className={className}
+          onClick={(event) =>
+            handleAddToStarred(
+              event,
+              btoa(titleId)
+                .replace(/=/g, "")
+                .replace(/\+/g, "-")
+                .replace(/\//g, "_")
+            )
+          }
+          data-tooltip={`Mark as starred`}
+          data-flow="up"
+        >
+          <span>
+            {/* Display a loading spinner if the button is in a loading state */}
+            {isLoading ? (
+              <FontAwesomeIcon icon={["fas", "circle-notch"]} size="lg" pulse />
+            ) : (
+              <FontAwesomeIcon icon={["far", "star"]} size="lg" />
+            )}
+          </span>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Star;
---
+++ src/lib/api/moviebunkers/methods/removeFromStarredTitles.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { moviebunkersAPI } from "..";
+
+export async function removeFromStarredTitles({ mbdbTitleId, source = { token: null } }) {
+    try {
+        // Make the API request to remove title from users fav list ?
+        const res = await moviebunkersAPI().post(`/userdata/remove-from-starred/${mbdbTitleId}`, {
+            cancelToken: source.token
+        })
+
+        if (res?.data) {
+            return res.data;
+        } else {
+            throw new Error('Error removing from starred')
+        }
+
+    } catch (error) {
+        // Get error message from response, or use a generic message.
+        let message = '';
+
+        // Handle errors by returning the appropriate message
+        if (axios.isCancel(error)) {
+            message = 'cancelled'
+        } else {
+            message = error?.response?.data?.error?.message ??
+                error?.message ??
+                "Something went wrong";
+        }
+
+        throw new Error(message);
+    }
+}
