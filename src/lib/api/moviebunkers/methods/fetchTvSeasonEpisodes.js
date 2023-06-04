d19d6de35b89f479724fa61737988b5d3319d886
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-04 11:38:34 +0530

    fetch tv seasons api method

---
+++ src/lib/api/moviebunkers/methods/fetchTvSeasonEpisodes.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { moviebunkersAPI } from "..";
+
+export async function fetchTvSeasonEpisodes({ titleId, seasonId, auth, source = { token: null } }) {
+    try {
+        // Make the API request to fetch tv sesason episodes
+        const res = await moviebunkersAPI(auth).get(`/episodes/tv/${titleId}/season/${seasonId}`, {
+            cancelToken: source.token
+        })
+
+        if (res?.data) {
+            return res.data;
+        } else {
+            throw new Error('Error fetching tv season episodes from mbdb')
+        }
+
+    } catch (error) {
+        // Get error message from response, or use a generic message.
+        let message = '';
+
+        // Handle errors by returning the appropriate message
+        if (axios.isCancel(error)) {
+            message = 'cancelled'
+        } else {
+            message = error?.response?.data?.error?.message ??
+                error?.message ??
+                "Something went wrong";
+        }
+
+        throw new Error(message);
+    }
+}
