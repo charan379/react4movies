42d1ddfb7a26f5c2d41d4089308017f13f904ce6
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-07 21:18:47 +0530

    convert string to url component

---
+++ src/lib/utils/makeURLComponent.js
@@ -0,0 +1,14 @@
+function makeURLComponent(url) {
+    // Replace any non-alphanumeric characters with a hyphen
+    url = url.replace(/[^a-zA-Z0-9]/g, "-");
+
+    // Convert all characters to lowercase
+    url = url.toLowerCase();
+
+    // Add slashes to separate the different parts of the URL
+    url = url.replace(/^-+/, "").replace(/-+$/, "").replace(/-+/g, "-");
+
+    return url;
+}
+
+export default makeURLComponent;
