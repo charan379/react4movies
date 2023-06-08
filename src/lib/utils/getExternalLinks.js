15544bac9448139680108288bec348c55e19fb05
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-08 13:30:58 +0530

    bug fixes

--- src/lib/utils/getExternalLinks.js
+++ src/lib/utils/getExternalLinks.js
@@ -1,12 +1,12 @@
 
 export const getExternalLinks = (title) => {
     const externalLinks = [];
-    if (title?.imdbId) {
-        externalLinks.push({ destination: 'IMDB', path: `https://www.imdb.com/title/${title?.imdbId}` })
+    if (title?.imdbId || title?.imdb_id) {
+        externalLinks.push({ destination: 'IMDB', path: `https://www.imdb.com/title/${title?.imdbId ?? title?.imdb_id}` })
     }
 
-    if (title?.tmdbId) {
-        externalLinks.push({ destination: 'TMDB', path: `https://www.themoviedb.org/${title?.titleType}/${title?.tmdbId}` })
+    if (title?.tmdbId || title?.tmdb_id) {
+        externalLinks.push({ destination: 'TMDB', path: `https://www.themoviedb.org/${title?.titleType ?? title?.title_type}/${title?.tmdbId ?? title?.tmdb_id}` })
     }
 
     return externalLinks;
