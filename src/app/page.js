2e4ea6a783fba8551e54d18ba05810a16f8aee5d
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-09 22:21:49 +0530

    db links added to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import styles from './page.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+      <div className={styles.links}>
+        <Link className={styles.link} href={"/titles/mbdb"}>
+          Mbdb Movies
+        </Link>
+        <Link className={styles.link} href={"/titles/tmdb"}>
+          Tmdb Movies
+        </Link>
+      </div>
     </main>
   )
 }
