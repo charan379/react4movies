21ce56ee9cf6b889c45d2616ed17cefde226ccba
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-10 17:53:46 +0530

    pass query params to api request

--- src/lib/api/moviebunkers/methods/fetchTvSeasons.js
+++ src/lib/api/moviebunkers/methods/fetchTvSeasons.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { moviebunkersAPI } from "..";
 
-export async function fetchTvSeasons({ titleId, auth, source = { token: null } }) {
+export async function fetchTvSeasons({ titleId, auth, queryParams = { limit: 0, sort_by: 'air_date.desc' }, source = { token: null } }) {
     try {
         // Make the API request to fetch tv sesasons
         const res = await moviebunkersAPI(auth).get(`/seasons/tv/${titleId}`, {
+            params: queryParams,
             cancelToken: source.token
         })
 
