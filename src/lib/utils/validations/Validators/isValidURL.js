a8ae2d07ba1fa23ea90230e4f97d4565e1880219
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-11 19:00:30 +0530

    url regex upadated to match ftp links

--- src/lib/utils/validations/Validators/isValidURL.js
+++ src/lib/utils/validations/Validators/isValidURL.js
@@ -1,5 +1,5 @@
 export default function isValidURL(url) {
     // Regular expression pattern for URL validation
-    var pattern = new RegExp('^https?:\\/\\/([a-z0-9\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w\\.-]*)*\\/?$', 'i');
+    var pattern = new RegExp('^(https?|ftp):\\/\\/(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,})(:\\d+)?([\\/\\?#].*)?$', 'i');
     return pattern.test(url);
 }
