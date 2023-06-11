1afba17a4a82adf64c55793b415dfe0833f2a60b
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-11 15:25:38 +0530

    link validators

---
+++ src/lib/utils/validations/link.js
@@ -0,0 +1,10 @@
+import isNonEmptyString from "./Validators/isNonEmptyString";
+import isValidURL from "./Validators/isValidURL";
+
+
+export default linkValidations = {
+    validateUrl: isValidURL,
+    validStringInput: isNonEmptyString,
+
+}
+
