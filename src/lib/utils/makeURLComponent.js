b255b1767a153f4cefba58b10f3fb8864a732574
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-12 20:09:09 +0530

    minor bug fixes

--- src/lib/utils/makeURLComponent.js
+++ src/lib/utils/makeURLComponent.js
@@ -1,14 +1,20 @@
 function makeURLComponent(url) {
-    // Replace any non-alphanumeric characters with a hyphen
-    url = url.replace(/[^a-zA-Z0-9]/g, "-");
+    try {
+        let newUrl;
+        // Replace any non-alphanumeric characters with a hyphen
+        newUrl = url.replace(/[^a-zA-Z0-9]/g, "-");
 
-    // Convert all characters to lowercase
-    url = url.toLowerCase();
+        // Convert all characters to lowercase
+        newUrl = url.toLowerCase();
 
-    // Add slashes to separate the different parts of the URL
-    url = url.replace(/^-+/, "").replace(/-+$/, "").replace(/-+/g, "-");
+        // Add slashes to separate the different parts of the URL
+        newUrl = url.replace(/^-+/, "").replace(/-+$/, "").replace(/-+/g, "-");
 
-    return url;
+        return newUrl;
+    } catch (error) {
+        console.log(error);
+        return url;
+    }
 }
 
 export default makeURLComponent;
