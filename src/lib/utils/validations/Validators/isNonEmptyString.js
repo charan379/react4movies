81996513af94b548a79e78deb75293e92df30969
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-17 18:29:28 +0530

    added min length param for validation

--- src/lib/utils/validations/Validators/isNonEmptyString.js
+++ src/lib/utils/validations/Validators/isNonEmptyString.js
@@ -1,3 +1,3 @@
-export default function isNonEmptyString(str, maxLength) {
-    return typeof str === 'string' && str.trim().length > 0 && str.length <= maxLength;
+export default function isNonEmptyString(str, maxLength, minLength = 1) {
+    return typeof str === 'string' && str.trim().length > 0 && str.length <= maxLength && str.length >= minLength;
 }
