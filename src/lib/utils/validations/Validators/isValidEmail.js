b0fdcd266132e3ec86faa2131c72e061f01c7a90
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-17 18:29:36 +0530

    email validator

---
+++ src/lib/utils/validations/Validators/isValidEmail.js
@@ -0,0 +1,6 @@
+export default function isValidEmail(email) {
+    // Email Validation as per RFC2822 standards
+    // Regular expression pattern for email validation
+    var pattern = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
+    return pattern.test(email);
+}
