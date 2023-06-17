edc3418aaf8345a0528773b6a926851739f5ee6b
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-17 20:40:46 +0530

    metadata updated

--- src/app/(user)/login/page.jsx
+++ src/app/(user)/login/page.jsx
@@ -5,18 +5,37 @@ export async function generateMetadata({ params }) {
   return {
     title: "Login | React4Movies",
     description: "Login into react4movies",
+    url: `${process.env.NEXTAUTH_URL}/login`,
     openGraph: {
       title: "Login",
       description: "Login into react4movies",
-      url: process.env.NEXTAUTH_URL,
+      url: `${process.env.NEXTAUTH_URL}/login`,
       siteName: "React4Movies",
       locale: "en-US",
       type: "website",
+      images: [
+        {
+          url: "/images/website-ss-256x256.jpg",
+          width: 185,
+          height: 278,
+        },
+        {
+          url: "/images/website-ss-256x256.jpg",
+          width: 342,
+          height: 513,
+        },
+        {
+          url: "/images/website-ss.jpg",
+          width: 500,
+          height: 750,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: "Login",
       description: "Login into react4movies",
+      images: ["/images/website-ss-256x256.jpg"],
     },
   };
 }
--- src/app/(user)/signup/page.jsx
+++ src/app/(user)/signup/page.jsx
@@ -5,18 +5,37 @@ export async function generateMetadata({ params }) {
   return {
     title: "Sign Up | React4Movies",
     description: "Sign Up into react4movies",
+    url: `${process.env.NEXTAUTH_URL}/signup`,
     openGraph: {
       title: "Sign Up | React4Movies",
       description: "Sign Up into react4movies",
-      url: process.env.NEXTAUTH_URL,
+      url: `${process.env.NEXTAUTH_URL}/signup`,
       siteName: "React4Movies",
       locale: "en-US",
       type: "website",
+      images: [
+        {
+          url: "/images/website-ss-256x256.jpg",
+          width: 185,
+          height: 278,
+        },
+        {
+          url: "/images/website-ss-256x256.jpg",
+          width: 342,
+          height: 513,
+        },
+        {
+          url: "/images/website-ss.jpg",
+          width: 500,
+          height: 750,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title: "Sign Up | React4Movies",
       description: "Sign Up into react4movies",
+      images: ["/images/website-ss-256x256.jpg"],
     },
   };
 }
