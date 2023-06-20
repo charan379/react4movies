86190e74b71089570cf5f6f302d2e68c2b510ddf
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-20 22:27:10 +0530

    bug fix

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-// import React from "react";\
+import React from "react";
+import styles from "./InfoCard.module.css";
 // font awesome library
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +9,6 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 //
-import styles from "./infoCard.module.css";
 import { useDisableBodyScrollOnModalOpen } from "@/lib/hooks/useDisableBodyScrollOnModalOpen";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
