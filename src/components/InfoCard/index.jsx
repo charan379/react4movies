78c257513d25f2b2fd8343ee1adc058e05b7f0eb
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-20 22:17:57 +0530

    info card added

---
+++ src/components/InfoCard/index.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+// import React from "react";\
+// font awesome library
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+//
+import styles from "./infoCard.module.css";
+import { useDisableBodyScrollOnModalOpen } from "@/lib/hooks/useDisableBodyScrollOnModalOpen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+const InfoCard = ({ message, link, linkText, open, close }) => {
+  // disable body scroll when modal is opened
+  useDisableBodyScrollOnModalOpen(open);
+
+  const handleClose = (event) => {
+    event.preventDefault();
+    close();
+  };
+
+  // If the `open` prop is false, don't render anything
+  if (!open) return null;
+
+  return (
+    <div className={`modal-container`}>
+      {/*  */}
+      <div className={styles.infoCard}>
+        {/* Close button */}
+        {close && (
+          <div className={styles.close}>
+            <button
+              data-tooltip={`Close`}
+              data-flow="left"
+              onClick={handleClose}
+              id="title-modal-close-btn"
+              className={styles.closeBtn}
+              tabIndex="0"
+            >
+              <FontAwesomeIcon icon={["fas", "times"]} size="lg" />
+            </button>
+          </div>
+        )}
+        {/*  */}
+        <div className={styles.tickMark}></div>
+        {/*  */}
+        {message && <div className={styles.message}>{message}</div>}
+        {/*  */}
+        {link && linkText && (
+          <Link className={styles.link} href={link ?? "/signup"}>
+            {linkText ?? ""}
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
