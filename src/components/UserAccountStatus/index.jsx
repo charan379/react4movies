484fa1e62ed09b3a62f54a35004ffa9f13b317fe
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-23 21:56:37 +0530

    userAccount status comp updated

--- src/components/UserAccountStatus/index.jsx
+++ src/components/UserAccountStatus/index.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import styles from "./UserAccountStatus.module.css";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { fetchUserAccountStatus } from "@/lib/api/moviebunkers/methods/fetchUserAccountStatus";
 import BarsLoadingAnimation from "../BarsLoadingAnimation";
 import axios from "axios";
+
+const VerificationForm = React.lazy(() => import("../UserVerificationForm"));
 
 const UserAccountStatus = (props) => {
   //
@@ -83,78 +85,101 @@ const UserAccountStatus = (props) => {
   //
   return (
     <div className={styles.userAccountStatus}>
-      <form className={styles.searchForm} onSubmit={handleSearchSubmit}>
-        <label className={styles.searchLabel}>
-          Search by:
-          <select
-            className={styles.searchOption}
-            value={searchOption}
-            onChange={handleSearchOptionChange}
-          >
-            <option value="userName">Username</option>
-            <option value="email">Email</option>
-          </select>
-        </label>
-        <input
-          className={styles.searchInput}
-          type={searchOption === "userName" ? "text" : "email"}
-          placeholder={`Search user account by ${searchOption}...`}
-          value={searchText}
-          onChange={handleSearchChange}
-        />
-        <button className={styles.searchButton} type="submit">
-          Search 🚀
-        </button>
-      </form>
-
+      {/*  */}
+      <div className={styles.topSection}>
+        {/*  */}
+        <form className={styles.searchForm} onSubmit={handleSearchSubmit}>
+          <label className={styles.searchLabel}>
+            Search by:
+            <select
+              className={styles.searchOption}
+              value={searchOption}
+              onChange={handleSearchOptionChange}
+            >
+              <option value="userName">Username</option>
+              <option value="email">Email</option>
+            </select>
+          </label>
+          <input
+            className={styles.searchInput}
+            type={searchOption === "userName" ? "text" : "email"}
+            placeholder={`Search user account by ${searchOption}...`}
+            value={searchText}
+            onChange={handleSearchChange}
+          />
+          <button className={styles.searchButton} type="submit">
+            Search 🚀
+          </button>
+        </form>
+      </div>
+      {/*  */}
       {isLoading && <BarsLoadingAnimation />}
-      {userAccount?.userName && (
+      {/*  */}
+      {userAccount?.userName && !isLoading ? (
         <>
-          <div className={styles.welcomeMessageSection}>
-            {/* say hi */}
-            <h2>{`Hi, ${userAccount?.userName}💞`}</h2>
-            {/* Inactive */}
-            {userAccount?.status === "Inactive" && (
-              <p className={styles.inactiveMessage}>
-                {`Congratulations!💐Your account has been successfully created. Please
+          {/* LEFT SECTION */}
+          <div className={styles.leftSection}>
+            <div className={styles.welcomeMessageSection}>
+              {/* say hi */}
+              <h2>{`Hi, ${userAccount?.userName}💞`}</h2>
+              {/* Inactive */}
+              {userAccount?.status === "Inactive" && (
+                <p className={styles.inactiveMessage}>
+                  {/* {`Congratulations!💐Your account has been successfully created. Please
                         await activation from our team. Once your account is activated🔓,
                         you'll be able to access all the features and benefits of our
+                        website💻. Thank you for joining us🤗`} */}
+                  {`Congratulations!💐Your account has been successfully created. Please
+                        verify your email account for account activation. Once your account is activated🔓,
+                        you'll be able to access all the features and benefits of our
                         website💻. Thank you for joining us🤗`}
-              </p>
+                </p>
+              )}
+              {/* Active */}
+              {userAccount?.status === "Active" && (
+                <p className={styles.inactiveMessage}>
+                  {`Congratulations!💐Your account has been successfully created and activated😊. Now, you will be able to access all the features and benefits of our website💻. Thank you for joining us and welcome aboard!🤗`}
+                </p>
+              )}
+            </div>
+            {/*  */}
+            <div
+              className={`${styles.userCard}`}
+              data-status={userAccount?.status.toString().toLowerCase()}
+            >
+              <h2 className={styles.status}>{userAccount.status}</h2>
+              <h3 className={styles.username}>{userAccount.userName}</h3>
+            </div>
+
+            {userAccount?.status === "Inactive" && (
+              <>
+                <p className={styles.inactiveMessage}>
+                  Your account seems to be Inactive / Not activated yet🔐.
+                </p>
+                <p className={styles.inactiveMessage}>
+                  Incase facing any issues,Please follow up with our team🤖 for
+                  account activation.
+                </p>
+              </>
             )}
-            {/* Active */}
-            {userAccount?.status === "Active" && (
-              <p className={styles.inactiveMessage}>
-                {`Congratulations!💐Your account has been successfully created and activated😊. Now, you will be able to access all the features and benefits of our website💻. Thank you for joining us and welcome aboard!🤗`}
-              </p>
+          </div>
+          {/* RIGHT SECTION  */}
+          <div className={styles.rightSection}>
+            {userAccount?.userName && (
+              <Suspense fallback={<BarsLoadingAnimation />}>
+                <VerificationForm
+                  user={{ ...userAccount }}
+                  setUserAccount={(data) => setUserAccount({ ...data })}
+                />
+              </Suspense>
             )}
           </div>
         </>
-      )}
-      {userAccount && !isLoading ? (
-        <div
-          className={`${styles.userCard}`}
-          data-status={userAccount?.status.toString().toLowerCase()}
-        >
-          <h2 className={styles.status}>{userAccount.status}</h2>
-          <h3 className={styles.username}>{userAccount.userName}</h3>
-        </div>
       ) : (
-        <>
+        <div className={styles.errorSection}>
           <p className={styles.noAccountMessage}>No user account found.</p>
           {error && <p className={styles.noAccountMessage}>{error}</p>}
-        </>
-      )}
-
-      {userAccount?.status === "Inactive" && (
-        <>
-          <p className={styles.inactiveMessage}>
-            Your account seems to be Inactive / Not activated yet🔐.
-          </p>
-          <p className={styles.inactiveMessage}>
-            Please follow up with our team🤖 for account activation.
-          </p>
-        </>
+        </div>
       )}
     </div>
   );
