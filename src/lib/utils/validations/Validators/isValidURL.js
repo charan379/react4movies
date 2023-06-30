4ee0e20c08c9ec607ba2ead9302011345c466be1
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-30 15:04:26 +0530

    added magnet validation

--- src/lib/utils/validations/Validators/isValidURL.js
+++ src/lib/utils/validations/Validators/isValidURL.js
@@ -1,5 +1,6 @@
 export default function isValidURL(url) {
     // Regular expression pattern for URL validation
     var pattern = new RegExp('^(https?|ftp):\\/\\/(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,})(:\\d+)?([\\/\\?#].*)?$', 'i');
-    return pattern.test(url);
+    var magentPattern = new RegExp(/magnet:\?xt=urn:(.*)/i);
+    return (pattern.test(url) || magentPattern.test(url));
 }
