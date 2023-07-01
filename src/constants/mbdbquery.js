31853df33944d432a0c993fd716a1abaefd4b816
charan379 <37008444+charan379@users.noreply.github.com>
2023-07-01 16:29:49 +0530

    sort by tmdb_id, ratting, runtime

--- src/constants/mbdbquery.js
+++ src/constants/mbdbquery.js
@@ -5,6 +5,13 @@ export const mbdbQuerySortOptions = [
     { label: "Year Asc", value: "year.asc" },
     { label: "Added Desc", value: "createdAt.desc" },
     { label: "Added Asc", value: "createdAt.asc" },
+    { label: "Ratting Asc", value: "ratting.asc" },
+    { label: "Ratting Desc", value: "ratting.desc" },
+    { label: "TMDB ID Asc", value: "tmdb_id.asc" },
+    { label: "TMDB ID Desc", value: "tmdb_id.desc" },
+    { label: "Runtime Asc", value: "runtime.asc" },
+    { label: "Runtime Desc", value: "runtime.desc" },
+    
 ];
 
 export const mbdbQueryPageResultsOptions = [
