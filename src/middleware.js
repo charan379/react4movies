dfc1d0f5f8242d12bf663d7cdd0ce0c41b37c3f2
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-27 16:56:04 +0530

    re-direct to login page after 8hrs of login

--- src/middleware.js
+++ src/middleware.js
@@ -1,29 +1,55 @@
 import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
-import { LevelOne, LevelThere } from "./constants/AuthRoles";
+import { LevelThere } from "./constants/AuthRoles";
 
-LevelOne
 export default withAuth(
     // `withAuth` augments your `Request` with the user's token.
     function middleware(request) {
         // console.log("token: ", request.nextauth.token);
 
-        if (request.nextUrl.pathname.startsWith("/admin") && !LevelThere.includes(request.nextauth.token?.role))
+        if(request.nextUrl.pathname === "/") {
+            return NextResponse.redirect(
+                new URL(`/titles/mbdb`, request?.url)
+            );
+        };
+
+        const token = request.nextauth.token;
+
+        // if no token then re-direct to login
+        if(!token || !token?.loginDate || !token?.accessToken) {
+            return NextResponse.redirect(
+                new URL(`/login?message=Un Authenticated User!&callbackUrl=${encodeURIComponent(request.nextUrl.href)}`, request?.url)
+            );
+        };
+
+        // if 8 hours completed from time of login then re-login
+        if(new Date() > new Date(token?.loginDate).getTime() + 8 * 60 * 60 * 1000) {
+            return NextResponse.redirect(
+                new URL(`/login?message=Authentication Expired!&callbackUrl=${encodeURIComponent(request.nextUrl.href)}`, request?.url)
+            );
+        }
+
+        if (request.nextUrl.pathname.startsWith("/admin") && !LevelThere.includes(token?.role))
             return NextResponse.redirect(
                 new URL(`/login?message=You Are Not Authorized!&callbackUrl=${encodeURIComponent(request.nextUrl.href)}`, request?.url)
-            );
-        if (request.nextUrl.pathname.startsWith("/user") && !LevelOne.includes(request.nextauth.token?.role))
-            return NextResponse.redirect(
-                new URL("/login?message=You Are Not Authorized!", request?.url)
             );
     },
     {
         callbacks: {
-            authorized: ({ token }) => !!token,
+            authorized: ({ token, req }) => !!token,
         },
     }
 );
 
 export const config = {
-    matcher: ["/admin/:path*", "/user/:path*"],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|login|signup|password-reset|user-account-status|\/|_next/static|_next/image|favicon.ico).*)',
+    ],
 };
