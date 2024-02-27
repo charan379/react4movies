324a4a4d4a27d6ddf82d24ab9c8c02df78f3c9a7
charan379 <37008444+charan379@users.noreply.github.com>
2024-02-27 18:51:54 +0530

    added new sort options

--- src/constants/mbdbquery.js
+++ src/constants/mbdbquery.js
@@ -5,6 +5,18 @@ export const mbdbQuerySortOptions = [
     { label: "Year Asc", value: "year.asc" },
     { label: "Added Desc", value: "createdAt.desc" },
     { label: "Added Asc", value: "createdAt.asc" },
+    { label: "Imdb Id Asc", value: "imdb_id.asc" },
+    { label: "Imdb Id Desc", value: "imdb_id.desc" },
+    { label: "Title Asc", value: "title.asc" },
+    { label: "Title Desc", value: "title.desc" },
+    { label: "Tmdb Id Asc", value: "tmdb_id.asc" },
+    { label: "Tmdb Id Desc", value: "tmdb_id.desc" },
+    { label: "Runtime Asc", value: "runtime.asc" },
+    { label: "Runtime Desc", value: "runtime.desc" },
+    { label: "Ratting Asc", value: "ratting.asc" },
+    { label: "Ratting Desc", value: "ratting.desc" },
+
+    
 ];
 
 export const mbdbQueryPageResultsOptions = [
